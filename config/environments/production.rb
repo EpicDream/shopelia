332546f8d8c60c6b66fc6d09d4e37954f7080d73
Amine Bellakrid <a.bellakrid@gmail.com>
2013-05-27 09:47:01 +0200

    force ssl just in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,9 @@ Shopelia::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
+  # Always use SSL
+  config.force_ssl = true
+
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
