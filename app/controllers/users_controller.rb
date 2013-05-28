6b077581c99499cec3d0d3d1d7d2e13b3d7d274d
Amine Bellakrid <a.bellakrid@gmail.com>
2013-05-28 15:56:12 +0200

    Adding tests for users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,24 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
 
   def edit
+    respond_to do |format|
+      format.html # edit.html.erb
+      format.json { render json: current_user }
+    end
   end
 
   def update
     if current_user.update_attributes(params[:user])
       flash[:notice] = "Vos modifications ont bien été enregistrées !"
-      redirect_to root_url
+      respond_to do |format|
+        format.html {redirect_to root_url}
+        format.json { head :no_content}
+      end
     else
-      render 'edit'
+      respond_to do |format|
+        format.html { render 'edit'}
+        format.json { render json: current_user.errors, status: :unprocessable_entity}
+      end
     end
   end
 
