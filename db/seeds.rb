0fecf0039772881b167695b5d64fc89e1961fbec
Eric Larchevêque <elarch@gmail.com>
2013-05-30 15:04:57 +0200

    Add new merchants

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,10 @@
 Psp.delete_all
 Psp.create(:id=>1,:name=>Psp::LEETCHI)
 Merchant.delete_all
-Merchant.create(:id=>1,:name=>'Rue du Commerce',:url=>'http://www.rueducommerce.fr',:tc_url=>'http://www.rueducommerce.fr/home/index.cfm/aide/cgv.htm',:logo=>'http://www.prixing.fr/images/logos/rueducommerce.png',:vendor=>'RueDuCommerce')
-Merchant.create(:id=>2,:name=>'Amazon France',:url=>'http://www.amazon.fr',:tc_url=>'http://www.amazon.fr/gp/help/customer/display.html?ie=UTF8&nodeId=548524',:logo=>'http://www.prixing.fr/images/logos/amazon.png',:vendor=>'AmazonFrance')
+Merchant.create(:id=>1,:name=>'Rue du Commerce',:url=>'http://www.rueducommerce.fr',:logo=>'http://www.prixing.fr/images/logos/rueducommerce.png',:vendor=>'RueDuCommerce')
+Merchant.create(:id=>2,:name=>'Amazon France',:url=>'http://www.amazon.fr',:logo=>'http://www.prixing.fr/images/logos/amazon.png',:vendor=>'AmazonFrance')
+Merchant.create(:id=>3,:name=>'Fnac',:url=>'http://www.fnac.fr',:logo=>'http://www.prixing.fr/images/logos/fnac.png',:vendor=>'Fnac')
+Merchant.create(:id=>4,:name=>'Price Minister',:url=>'http://www.priceminister.fr',:logo=>'http://www.prixing.fr/images/logos/priceminister.png',:vendor=>'PriceMinister')
 Developer.delete_all
 Developer.create(:name=>'Prixing',:api_key=>'52953f1868a7545011d979a8c1d0acbc310dcb5a262981bd1a75c1c6f071ffb4')
 Country.delete_all
