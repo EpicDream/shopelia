eb1df120b5ab252836e0beeade1dcc064b5ac51b
Eric Larchevêque <elarch@gmail.com>
2013-06-01 15:11:29 +0200

    Sortable helper

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,11 +1,22 @@
 class Admin::OrdersController < Admin::AdminController
-
+  helper_method :sort_column, :sort_direction
+  
   def index
-    @orders = Order.page(params[:page]).order('created_at DESC')
+    @orders = Order.page(params[:page]).order(sort_column + " " + sort_direction)
   end
 
   def show
     @order = Order.find_by_uuid!(params[:id])
+  end
+  
+  private
+  
+  def sort_column
+    Order.column_names.include?(params[:sort]) ? params[:sort] : "created_at"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
   end
   
 end
