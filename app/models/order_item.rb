eeb0a1ba84f01e624072f3d15618a19ca3aced6b
Eric Larchevêque <elarch@gmail.com>
2013-07-08 16:00:54 +0200

    Truncate order items product title to 250 chars

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -8,4 +8,12 @@ class OrderItem < ActiveRecord::Base
   alias_attribute :price_product, :product_price
   alias_attribute :price_delivery, :shipping_price
   alias_attribute :delivery_text, :shipping_info
+  
+  before_save :truncate_product_title
+  
+  private
+  
+  def truncate_product_title
+    self.product_title = self.product_title[0..249] if self.product_title && self.product_title.length > 250
+  end
 end
