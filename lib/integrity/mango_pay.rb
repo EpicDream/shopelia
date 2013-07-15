49edc09aa1779bc3dba1ff6bd0371c0907e66d79
Eric Larchevêque <elarch@gmail.com>
2013-07-15 14:45:32 +0200

    Fix array comparison

--- lib/integrity/mango_pay.rb
+++ lib/integrity/mango_pay.rb
@@ -33,7 +33,7 @@ module Integrity
         end
       end
       contribution_ids_B = Order.completed.where("created_at >= ?", 31.days.ago.to_date.to_s).where(billing_solution:'mangopay').map(&:mangopay_contribution_id)
-      if contribution_ids_A.to_a != contribution_ids_B.to_a
+      if contribution_ids_A.to_set != contribution_ids_B.to_set
         log << "Inconsistent contribution_ids ! REPORT:#{contribution_ids_A} vs DB:#{contribution_ids_B}"
       end
       log
