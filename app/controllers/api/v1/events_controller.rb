abd0697ce5dc1e232abac0fcb132d85d7e313b6e
Eric Larchevêque <elarch@gmail.com>
2013-07-17 19:50:32 +0200

    Allow setting of visitor, fix action

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -7,6 +7,7 @@ class Api::V1::EventsController < Api::V1::BaseController
   api :POST, "/api/events", "Create events"
   param :urls, Array, "Urls of the products", :required => true
   param :tracker, String, "Tracker", :required => false
+  param :visitor, String, "Visitor UUID", :required => false
   def create
     Event.from_urls(
       :urls => params[:urls],
@@ -23,14 +24,18 @@ class Api::V1::EventsController < Api::V1::BaseController
   
   def prepare_params
     @tracker = params[:tracker]
-    @action = params[:action].eql?("click") ? Event::CLICK : Event::VIEW
+    @action = params[:type].eql?("click") ? Event::CLICK : Event::VIEW
   end
     
   def set_visitor_cookie 
-    @visitor = cookies[:visitor]
-    if @visitor.nil?
-      @visitor = SecureRandom.hex(16)
-      cookies[:visitor] = { :value => @visitor, :expires => 10.years.from_now }
+    if params[:visitor]
+      @visitor = params[:visitor]
+    else
+      @visitor = cookies[:visitor]
+      if @visitor.nil?
+        @visitor = SecureRandom.hex(16)
+        cookies[:visitor] = { :value => @visitor, :expires => 10.years.from_now }
+      end
     end
   end
   
--- test/functional/api/v1/events_controller_test.rb
+++ test/functional/api/v1/events_controller_test.rb
@@ -5,9 +5,8 @@ class Api::V1::EventsControllerTest < ActionController::TestCase
 
   test "it should create events from list of urls" do
     assert_difference("Event.count", 2) do
-      post :create, urls:["http://www.amazon.fr/1","http://www.amazon.fr/1"], action:"view", tracker:"toto", format: :json
+      post :create, urls:["http://www.amazon.fr/1","http://www.amazon.fr/1"], tracker:"toto", visitor:"1234", format: :json
     end
-    assert_equal 32, cookies[:visitor].length
     assert_equal developers(:prixing).api_key, cookies[:developer_key]
     
     event = Event.all.first
@@ -16,6 +15,18 @@ class Api::V1::EventsControllerTest < ActionController::TestCase
     assert_equal developers(:prixing).id, event.developer_id
     assert_equal "0.0.0.0", event.ip_address
     assert_equal "toto", event.tracker
+    assert_equal "1234", event.visitor
+  end
+
+  test "it should create events from list of urls and with action type" do
+    assert_difference("Event.count", 2) do
+      post :create, urls:["http://www.amazon.fr/1","http://www.amazon.fr/1"], type:"click", format: :json
+    end
+    assert_equal 32, cookies[:visitor].length
+    
+    event = Event.all.first
+    assert_equal 1, event.action
+    assert_equal 32, event.visitor.length
   end
 
 end
