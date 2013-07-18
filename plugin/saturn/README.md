f626a15a615f05b52c9406c28ab15d79a611c3eb
Vincent Renaudineau <vincent.renaudineau@prixing.fr>
2013-07-18 13:03:12 +0200

    [Saturn] first commit

---
+++ plugin/saturn/README.md
@@ -0,0 +1,84 @@
+
+Saturn Chromium Extension
+=========================
+
+Saturn is a Chromium plugin used to extract products informations.
+  
+He gets a product url from shopelia server, open it, extract all usefull informations and send them back to server.
+  
+He can extract for example :
+
+- title
+- price
+- price strikeout
+- shipping price
+- shipping info
+- availability
+- brand
+- description
+- a main image
+- other images
+
+Installation
+------------
+  
+In Chromium extensions' tab :
+1) Check 'Developper mode'
+2) Then click on 'Load unpacked extension'
+3) Browse on Saturn directory and open it.
+4) Check 'Allow in private navigation'.
+
+Usage
+-----
+  
+You have nothing to do, the extension ask itself for product to crawl.
+  
+You can crawl the product on the current page by clicking on the extension's button at top-right of the window.
+
+We recommand you to use it in a private tab, to do not be tracked.
+
+Developpers
+-----------
+
+There are two main files :
+
+- background.js
+- contentscript.js
+
+### background.js
+
+It's the logic piece of the program.
+It communicate with the server, load pages, and ask the content script to crawl the current page.
+
+Common scenario :
+
+1. background ask for a product to crawl ;
+2. it load the product url ;
+3. it load the content script ;
+4. it ask the content script for colors ;
+5. for each color :
+    6. it ask the content script to select the color ;
+    7. it ask the content script for the sizes ;
+    8. for each size :
+        9. it ask the content script to select the size ;
+        10. it ask the content script to extract all informations ;
+11. it send all product options to the server ;
+12. it restart to step 1.
+
+### contentscript.js
+
+It's the executive piece of the program.
+Only the contentscript can interact with the web page.
+But it can not communicate with other server, and it is reloaded each time the page is.
+
+To crawl the page, it need a mapping for this website.
+The mapping indicate where to find all elements on the page.
+The mapping must be previously set with Humanis extension.  
+
+### More Documentation
+
+[Chrome extensions, pour commencer.](http://developer.chrome.com/extensions/getstarted.html)
+[Chrome extensions, Background Pages.](http://developer.chrome.com/extensions/background_pages.html)
+[Chrome extensions, Content Scripts.](http://developer.chrome.com/extensions/content_scripts.html)
+
+[CSS selectors.](http://www.w3schools.com/cssref/css_selectors.asp)
---
+++ plugin/saturn/controllers/contentscript.js
@@ -0,0 +1,135 @@
+
+function getColors(mapping) {
+  var path = mapping.colors;
+  var e = document.querySelector(path);
+  if (! e)
+    return [];
+  else if (e.tagName == "SELECT") {
+    var options = e.querySelectorAll("option:enabled");  
+    var colors = _.map(options, function(opt) {return opt.innerText});
+  } else {
+    var images = e.querySelectorAll("img");
+    if (images.length == 0) return [];
+    var colors = _.chain(images).map(function(img) {return img.getAttribute("src");}).uniq().value();
+  }
+  return colors;
+};
+
+function setColor(mapping, color) {
+  var path = mapping.colors;
+  var e = document.querySelector(path);
+
+  if (e && e.tagName == "SELECT") {
+    var option = $(e).find("option:contains('"+color+"')");
+    option[0].selected = true;
+    option.parent().change();
+    return true;
+  } else if (e) {
+    $(e).find("img[src='"+color+"']").eq(0).click();
+    return true;
+  } else
+    console.error("Unknow tagname for element", e, "and path", path);
+  return false;
+};
+
+function getSizes(mapping) {
+  if (! mapping.sizes)
+    return [];
+  var path = mapping.sizes;
+  var e = document.querySelector(path);
+  if (! e)
+    return [];
+  else if (! e.tagName == "SELECT")
+    e = e.querySelector("select");
+
+  if (! e) 
+    return [];
+
+  var options = e.querySelectorAll("option:enabled");
+  var sizes = _.chain(options).map(function(opt) {return opt.innerText}).filter(function(size) {return size.match(/choi|choo/i) == null;}).value();
+  return sizes;
+};
+
+function setSize(mapping, size) {
+  var path = mapping.sizes;
+  var e = document.querySelector(path);
+  
+  if (e && e.tagName == "SELECT") {
+    var option = $(e).find("option:contains('"+size+"')");
+    option[0].selected = true;
+    option.parent().change();
+  } else {
+    console.error("Unknow tagname for element", e, "and path", path);
+    return false;
+  }
+  return true;
+};
+
+
+function crawl(mapping) {
+  var option = {};
+  var textFields = ['name', 'brand', 'description', 'price', 'price_strikeout', 'shipping_price', 'shipping_info', 'availibility'];
+  for (var i in textFields) {
+    var key = textFields[i];
+    var path = mapping[key];
+    if (! path) continue;
+    var e = document.querySelector(path);
+    if (! e) continue;
+    var value = e.innerText;
+    option[key] = value;
+  }
+  var imageFields = ['image_url', 'images'];
+  for (var i in imageFields) {
+    var key = imageFields[i];
+    var path = mapping[key];
+    if (! path) continue;
+    var e = document.querySelector(path);
+    if (! e) continue;
+    var images = e.querySelectorAll("img");
+    if (images.length == 0) continue;
+    var values = _.chain(images).map(function(img) {return img.getAttribute("src");}).uniq().value();
+    if (key == 'image_url')
+      values = values[0];
+    option[key] = values;
+  }
+  return option;
+};
+
+chrome.extension.onMessage.addListener(function(hash, sender, callback) {
+  if (sender.id != chrome.runtime.id) return;
+  var action = hash.action;
+  var mapping = hash.mapping;
+  var data = hash.data;
+  var result;
+  console.debug("ProductCrawl task received", hash);
+  switch(action) {
+    case "getColors":
+      result = getColors(mapping);
+      break;
+    case "setColor":
+      result = setColor(mapping, data);
+      return setTimeout(goNextStep, 500);
+      break;
+    case "getSizes":
+      result = getSizes(mapping);
+      break;
+    case "setSize":
+      result = setSize(mapping, data);
+      return setTimeout(goNextStep, 500);
+      break;
+    case "crawl":
+      result = crawl(mapping);
+      break;
+    default:
+      console.error("Unknow command", action);
+      result = false;
+  }
+  if (callback)
+    callback(result);
+});
+
+function goNextStep() {
+  chrome.extension.sendMessage("nextStep");
+};
+
+goNextStep();
