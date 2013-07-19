cf2b41bf44d08c16095d341bc80c5896f8c09e05
Shopelia <shopelia@shopelia.fr>
2013-07-19 19:54:43 +0200

    Cleanup

--- db/schema.rb
+++ db/schema.rb
@@ -128,9 +128,9 @@ ActiveRecord::Schema.define(:version => 20130719165044) do
     t.integer  "merchant_id"
     t.string   "uuid"
     t.string   "state_name"
-    t.text     "message",                       :limit => 255
-    t.datetime "created_at",                                   :null => false
-    t.datetime "updated_at",                                   :null => false
+    t.text     "message"
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
     t.string   "questions_json"
     t.string   "error_code"
     t.integer  "address_id"
@@ -152,7 +152,6 @@ ActiveRecord::Schema.define(:version => 20130719165044) do
     t.integer  "mangopay_contribution_id"
     t.string   "mangopay_contribution_status"
     t.integer  "mangopay_contribution_amount"
-    t.string   "payment_solution"
     t.string   "billing_solution"
     t.string   "injection_solution"
     t.string   "cvd_solution"
@@ -198,10 +197,10 @@ ActiveRecord::Schema.define(:version => 20130719165044) do
   create_table "products", :force => true do |t|
     t.string   "name"
     t.integer  "merchant_id"
-    t.text     "url",                 :limit => 255
+    t.text     "url"
     t.string   "image_url"
-    t.datetime "created_at",                         :null => false
-    t.datetime "updated_at",                         :null => false
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
     t.text     "description"
     t.text     "images"
     t.integer  "product_master_id"
