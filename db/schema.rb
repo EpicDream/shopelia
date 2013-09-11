9080be6ca1c9e2cb36ae81da4416b8254916c7f7
Shopelia <shopelia@shopelia.fr>
2013-09-11 14:49:26 +0200

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -62,9 +62,9 @@ ActiveRecord::Schema.define(:version => 20130909131321) do
     t.integer  "mangopay_contribution_id"
     t.integer  "mangopay_contribution_amount"
     t.string   "mangopay_contribution_message"
+    t.integer  "mangopay_destination_wallet_id"
     t.datetime "created_at",                     :null => false
     t.datetime "updated_at",                     :null => false
-    t.integer  "mangopay_destination_wallet_id"
     t.integer  "mangopay_transfer_id"
   end
 
@@ -210,9 +210,9 @@ ActiveRecord::Schema.define(:version => 20130909131321) do
     t.integer  "merchant_id"
     t.string   "uuid"
     t.string   "state_name"
-    t.text     "message",                    :limit => 255
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
+    t.text     "message"
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
     t.string   "questions_json"
     t.string   "error_code"
     t.integer  "retry_count"
@@ -228,7 +228,6 @@ ActiveRecord::Schema.define(:version => 20130909131321) do
     t.float    "billed_price_product"
     t.float    "billed_price_shipping"
     t.datetime "notification_email_sent_at"
-    t.string   "payment_solution"
     t.string   "injection_solution"
     t.string   "cvd_solution"
     t.integer  "developer_id"
@@ -273,10 +272,10 @@ ActiveRecord::Schema.define(:version => 20130909131321) do
     t.float    "price_strikeout"
     t.string   "shipping_info"
     t.text     "description"
-    t.datetime "created_at",                       :null => false
-    t.datetime "updated_at",                       :null => false
-    t.text     "option2",           :limit => 255
-    t.text     "option1",           :limit => 255
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
+    t.text     "option2"
+    t.text     "option1"
     t.string   "name"
     t.boolean  "available"
     t.text     "image_url"
@@ -295,10 +294,10 @@ ActiveRecord::Schema.define(:version => 20130909131321) do
   create_table "products", :force => true do |t|
     t.string   "name"
     t.integer  "merchant_id"
-    t.text     "url",                 :limit => 255
-    t.text     "image_url",           :limit => 255
-    t.datetime "created_at",                                            :null => false
-    t.datetime "updated_at",                                            :null => false
+    t.text     "url"
+    t.text     "image_url"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
     t.text     "description"
     t.integer  "product_master_id"
     t.string   "brand"
@@ -306,7 +305,7 @@ ActiveRecord::Schema.define(:version => 20130909131321) do
     t.boolean  "viking_failure"
     t.string   "reference"
     t.datetime "muted_until"
-    t.boolean  "options_completed",                  :default => false
+    t.boolean  "options_completed",   :default => false
   end
 
   create_table "states", :force => true do |t|
