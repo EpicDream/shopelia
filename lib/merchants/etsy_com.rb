12111288e580601b22bac2376e7cde0bb55d7039
Eric Larchevêque <elarch@gmail.com>
2013-09-27 09:54:06 +0200

    Fix Etsy shipping price parser

--- lib/merchants/etsy_com.rb
+++ lib/merchants/etsy_com.rb
@@ -7,22 +7,24 @@ class EtsyCom
 
   def process_shipping_price version
     text = version[:price_shipping_text].unaccent
-    version[:price_shipping_text] = (m = text.match(/France\s+€([\d,]+)/) || \
-      m = text.match(/Union europeenne\s+€([\d,]+)/) || \
-      m = text.match(/(?:^|,\s)UE\s+€([\d,]+)/) || \
-      m = text.match(/Autres pays\s+€([\d,]+)/)) ? m[1] : generate_incident(text)
+    version[:price_shipping_text] = (m = text.match(/France\s+€([\d,\.]+)/) || \
+      m = text.match(/Union europeenne\s+€([\d,\.]+)/) || \
+      m = text.match(/(?:^|,\s)UE\s+€([\d,\.]+)/) || \
+      m = text.match(/Autres pays\s+€([\d,\.]+)/)) ? m[1] : generate_incident(text)
     version      
   end
 
   private
 
   def generate_incident str
-    Incident.create(
-      :issue => "Viking",
-      :description => str,
-      :resource_type => "Merchant",
-      :resource_id => Merchant.find_by_domain("etsy.com").id,
-      :severity => Incident::IMPORTANT)
+    if str !~ /Etats-Unis/
+      Incident.create(
+        :issue => "Viking",
+        :description => str,
+        :resource_type => "Merchant",
+        :resource_id => Merchant.find_by_domain("etsy.com").id,
+        :severity => Incident::IMPORTANT)
+    end
     nil
   end
 end
