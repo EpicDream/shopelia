a250f8274194f945461c05dbc4d0417cb470c504
Vincent Renaudineau <vincent.renaudineau@prixing.fr>
2013-09-27 11:04:45 +0200

    [Viking] new availability case

--- test/unit/product_version_test.rb
+++ test/unit/product_version_test.rb
@@ -165,7 +165,7 @@ class ProductVersionTest < ActiveSupport::TestCase
               "retrait gratuit en magasin", "dans plus de 50 magasins", "dans 48 magasins",
               "non disponible", "Désolés, cet article a été vendu. Vous aimerez peut-être ceci",
               "Mince alors. Cet article n'est plus disponible.", "Ce magasin est en vacances",
-              "ce produit n'est plus en stock" ]
+              "ce produit n'est plus en stock", "PAS DE CADEAUX INSOLITES ... CONTINUEZ VOTRE NAVIGATION" ]
     array.each do |str|
       version = ProductVersion.create(
         product_id:@product.id,
