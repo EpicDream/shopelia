6260d0bd469a1fd9024fc68bda6976131dd264c0
Vincent Renaudineau <vincent.renaudineau@prixing.fr>
2013-09-30 11:07:10 +0200

    [Viking] add new availability cases

--- test/unit/product_version_test.rb
+++ test/unit/product_version_test.rb
@@ -183,7 +183,8 @@ class ProductVersionTest < ActiveSupport::TestCase
   test "it should set available info to true" do
     array = [ "en stock", "8 offres", "en vente sur", "Précommandez maintenant pour réserver votre Kindle Paperwhite.",
               "Expédié habituellement sous 2 à 3 semaines", "Peu de stock", "Stock modéré",
-              "disponible sous 4 semaines", "Seulement 1 en stock" ]
+              "disponible sous 4 semaines", "Seulement 1 en stock", "in stock but may require an extra 1-2 days to process.",
+              "Conditions spéciales :- livraison : 10 semaines", "livraison des fichiers" ]
     array.each do |str|
       assert_difference "Incident.count", 0 do
         version = ProductVersion.create(
