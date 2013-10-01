c5bed47c2e7072d935f7690532e16f86523c6ba5
Shopelia <shopelia@shopelia.fr>
2013-10-01 17:41:59 +0200

    New rule for Amazon canonization

--- db/schema.rb
+++ db/schema.rb
@@ -62,9 +62,9 @@ ActiveRecord::Schema.define(:version => 20131001145339) do
     t.integer  "mangopay_contribution_id"
     t.integer  "mangopay_contribution_amount"
     t.string   "mangopay_contribution_message"
+    t.integer  "mangopay_destination_wallet_id"
     t.datetime "created_at",                     :null => false
     t.datetime "updated_at",                     :null => false
-    t.integer  "mangopay_destination_wallet_id"
     t.integer  "mangopay_transfer_id"
   end
 
@@ -86,8 +86,6 @@ ActiveRecord::Schema.define(:version => 20131001145339) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.string   "uuid"
-    t.integer  "kind"
   end
 
   create_table "cashfront_rules", :force => true do |t|
@@ -212,9 +210,9 @@ ActiveRecord::Schema.define(:version => 20131001145339) do
     t.integer  "merchant_id"
     t.string   "uuid"
     t.string   "state_name"
-    t.text     "message",                    :limit => 255
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
+    t.text     "message"
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
     t.string   "questions_json"
     t.string   "error_code"
     t.integer  "retry_count"
@@ -230,7 +228,6 @@ ActiveRecord::Schema.define(:version => 20131001145339) do
     t.float    "billed_price_product"
     t.float    "billed_price_shipping"
     t.datetime "notification_email_sent_at"
-    t.string   "payment_solution"
     t.string   "injection_solution"
     t.string   "cvd_solution"
     t.integer  "developer_id"
@@ -261,7 +258,6 @@ ActiveRecord::Schema.define(:version => 20131001145339) do
     t.datetime "updated_at",                   :null => false
     t.integer  "amount"
     t.integer  "mangopay_source_wallet_id"
-    t.integer  "virtual_card_id"
   end
 
   create_table "product_masters", :force => true do |t|
@@ -276,10 +272,10 @@ ActiveRecord::Schema.define(:version => 20131001145339) do
     t.float    "price_strikeout"
     t.string   "shipping_info"
     t.text     "description"
-    t.datetime "created_at",                       :null => false
-    t.datetime "updated_at",                       :null => false
-    t.text     "option2",           :limit => 255
-    t.text     "option1",           :limit => 255
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
+    t.text     "option2"
+    t.text     "option1"
     t.string   "name"
     t.boolean  "available"
     t.text     "image_url"
@@ -298,10 +294,10 @@ ActiveRecord::Schema.define(:version => 20131001145339) do
   create_table "products", :force => true do |t|
     t.string   "name"
     t.integer  "merchant_id"
-    t.text     "url",                 :limit => 255
-    t.text     "image_url",           :limit => 255
-    t.datetime "created_at",                                            :null => false
-    t.datetime "updated_at",                                            :null => false
+    t.text     "url"
+    t.text     "image_url"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
     t.text     "description"
     t.integer  "product_master_id"
     t.string   "brand"
@@ -309,7 +305,7 @@ ActiveRecord::Schema.define(:version => 20131001145339) do
     t.boolean  "viking_failure"
     t.string   "reference"
     t.datetime "muted_until"
-    t.boolean  "options_completed",                  :default => false
+    t.boolean  "options_completed",   :default => false
     t.datetime "viking_sent_at"
   end
 
@@ -382,17 +378,5 @@ ActiveRecord::Schema.define(:version => 20131001145339) do
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-
-  create_table "virtual_cards", :force => true do |t|
-    t.string   "provider"
-    t.string   "number"
-    t.string   "exp_month"
-    t.string   "exp_year"
-    t.string   "cvv"
-    t.float    "amount"
-    t.integer  "cvd_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
 
 end
