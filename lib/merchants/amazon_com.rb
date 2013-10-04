3af09623a9343e35c98c43705fcc1d810a9e18c7
Vincent Renaudineau <vincent.renaudineau@prixing.fr>
2013-10-04 16:01:38 +0200

    [Viking] improve amazon.com

--- lib/merchants/amazon_com.rb
+++ lib/merchants/amazon_com.rb
@@ -5,6 +5,11 @@ class AmazonCom
     @url = url
   end
 
+  def process_availability version
+    version[:availability_text] = "Non disponible" if version[:availability_text] =~ /\$\d+(\.\d+)? - \$\d+(\.\d+)?/
+    version
+  end
+
   def process_price version
     version[:price_text] = "0,00 €" if version[:price_text] == "Currently unavailable."
     version
