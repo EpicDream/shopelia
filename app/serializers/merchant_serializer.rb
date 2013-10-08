e7f7d9daf17da33ac78a9d6fac6931690be80a81
Shopelia <shopelia@shopelia.fr>
2013-10-08 13:02:53 +0200

    Fix

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,12 +1,8 @@
 class MerchantSerializer < ActiveModel::Serializer
-  attributes :id, :name, :logo, :url, :accepting_orders, :allow_iframe, :domain, :allow_quantities
+  attributes :id, :name, :logo, :url, :accepting_orders, :domain, :allow_quantities
   
   def accepting_orders
     object.accepting_orders? ? 1 : 0
-  end
-
-  def allow_iframe
-    object.allow_iframe? ? 1 : 0
   end
 
   def allow_quantities
