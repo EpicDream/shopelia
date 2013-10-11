b7b795fb69e4ac045d33d0d73fe9d94dac9b3a77
Shopelia <shopelia@shopelia.fr>
2013-10-11 21:39:14 +0200

    Fix to work in production env

--- db/schema.rb
+++ db/schema.rb
@@ -62,9 +62,9 @@ ActiveRecord::Schema.define(:version => 20131008083427) do
     t.integer  "mangopay_contribution_id"
     t.integer  "mangopay_contribution_amount"
     t.string   "mangopay_contribution_message"
+    t.integer  "mangopay_destination_wallet_id"
     t.datetime "created_at",                     :null => false
     t.datetime "updated_at",                     :null => false
-    t.integer  "mangopay_destination_wallet_id"
     t.integer  "mangopay_transfer_id"
   end
 
@@ -261,7 +261,6 @@ ActiveRecord::Schema.define(:version => 20131008083427) do
     t.datetime "updated_at",                   :null => false
     t.integer  "amount"
     t.integer  "mangopay_source_wallet_id"
-    t.integer  "virtual_card_id"
   end
 
   create_table "product_masters", :force => true do |t|
@@ -382,17 +381,5 @@ ActiveRecord::Schema.define(:version => 20131008083427) do
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-
-  create_table "virtual_cards", :force => true do |t|
-    t.string   "provider"
-    t.string   "number"
-    t.string   "exp_month"
-    t.string   "exp_year"
-    t.string   "cvv"
-    t.float    "amount"
-    t.integer  "cvd_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
 
 end
