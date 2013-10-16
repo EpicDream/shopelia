df32a11b87f9bd14dd7d3684fe9d021881f77469
Amine Bellakrid <a.bellakrid@gmail.com>
2013-10-16 17:55:07 +0200

    fix bootstrap for developers

--- app/assets/javascripts/developers/application.js
+++ app/assets/javascripts/developers/application.js
@@ -1,5 +1,5 @@
 //= require jquery
-//= require bootstrap
+//= require twitter/bootstrap
 //= require dataTables/jquery.dataTables
 //= require dataTables/jquery.dataTables.bootstrap
 //= require admin/datatables.reload_ajax
