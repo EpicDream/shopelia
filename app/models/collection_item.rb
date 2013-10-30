847839d7363052c416a9cdbf6792ecce3dc4c596
Eric Larchevêque <elarch@gmail.com>
2013-10-30 15:24:50 +0100

    Refactor collection items push

--- app/models/collection_item.rb
+++ app/models/collection_item.rb
@@ -11,6 +11,7 @@ class CollectionItem < ActiveRecord::Base
 
   before_validation :check_url_validity, if:Proc.new{ |item| item.url.present? }
   before_validation :find_or_create_product, if:Proc.new{ |item| item.url.present? && item.errors.empty? }
+  after_create :generate_event
 
   private
 
@@ -23,5 +24,14 @@ class CollectionItem < ActiveRecord::Base
   
   def find_or_create_product
     self.product_id = Product.fetch(self.url).id
+  end
+
+  def generate_event
+    self.product.authorize_push_channel
+    EventsWorker.perform_async({
+      :product_id => self.product_id,
+      :action => Event::REQUEST,
+      :tracker => "display-collection"
+    })
   end
 end
