86c006574f37dbc375bb6bf7b1301bb6a6277ed3
Amine Bellakrid <a.bellakrid@gmail.com>
2013-11-06 23:34:57 +0100

    Adding scroll to bottom when updating a message

--- app/assets/javascripts/admin/georges/devices.js
+++ app/assets/javascripts/admin/georges/devices.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
         $(this).toggleClass('user-row-select');
     });
 
+
+
   devicesTable = $('#devices').dataTable( {
     "sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
     "bServerSide": true,
