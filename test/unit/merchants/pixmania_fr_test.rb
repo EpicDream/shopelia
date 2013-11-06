41429d4d0f30f77660e9a662a555034729484c3c
Vincent Renaudineau <vincent.renaudineau@prixing.fr>
2013-11-06 16:22:04 +0100

    [Viking] add PixmaniaFr merchant helper

---
+++ test/unit/merchants/pixmania_fr_test.rb
@@ -0,0 +1,27 @@
+# -*- encoding : utf-8 -*-
+require 'test_helper'
+
+class PixmaniaFrTest < ActiveSupport::TestCase
+
+  setup do
+    @version = {}
+    @url = "http://www.pixmania.fr/bridge/nikon-coolpix-p520-noir/21169997-a.html"
+    @helper = PixmaniaFr.new(@url)
+  end
+
+  test "it should find class from url" do
+    assert MerchantHelper.send(:from_url, @url).kind_of?(PixmaniaFr)
+  end
+
+  test "it should process price shipping" do
+    @version[:price_shipping_text] = ""
+    @version = @helper.process_price_shipping(@version)
+    assert_equal PixmaniaFr::DEFAULT_PRICE_SHIPPING, @version[:price_shipping_text]
+  end
+
+  test "it should process shipping info" do
+    @version[:shipping_info] = ""
+    @version = @helper.process_shipping_info(@version)
+    assert_equal PixmaniaFr::DEFAULT_SHIPPING_INFO, @version[:shipping_info]
+  end
+end
