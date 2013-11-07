e0b7a6b6e400f6c6bf90b8ecf552378d2ae7c691
Eric Larchevêque <elarch@gmail.com>
2013-11-07 11:33:12 +0100

    Fix tradedoubler

--- test/unit/merchants/tradedoubler_com_test.rb
+++ test/unit/merchants/tradedoubler_com_test.rb
@@ -7,6 +7,9 @@ class TradedoublerComTest < ActiveSupport::TestCase
     urls = [
       { in: 'http://pdt.tradedoubler.com/click?a(2238732)p(72222)prod(117232539)ttid(5)url(http%3A%2F%2Fwww.cdiscount.com%2Fdp.asp%3Fsku%3D3596971684129%26refer%3D*)',
         out: 'http://www.cdiscount.com/dp.asp?sku=3596971684129'
+      },
+      { in: "http://pdt.tradedoubler.com/click?a(2056816)p(3436)prod(1182483695)ttid(5)url(http%3A%2F%2Fwww.yoox.com%2Fitem.asp%3Fcod10%3D35187059%26tp%3D14681%26utm_source%3Dtradedoubler_fr%26utm_medium%3Daffiliazione%26utm_campaign%3Daffiliazione_fr%26tskay%3D2FA4E164)",
+        out: nil
       }
     ]
     urls.each do |url|
@@ -14,4 +17,3 @@ class TradedoublerComTest < ActiveSupport::TestCase
     end
   end
 end
-
