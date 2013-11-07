4805adab1f64f1dc40d991123d18219140886f61
Manuel Valente <manuel.valente@gmail.com>
2013-11-07 18:06:23 +0100

    Added Webgains; Various fixes

--- app/models/algolia_tag.rb
+++ app/models/algolia_tag.rb
@@ -4,15 +4,5 @@ class AlgoliaTag < ActiveRecord::Base
 
   attr_accessible :count, :kind, :name
 
-  def self.build_from_redis
-    redis = Redis.new
-    AlgoliaTag.delete_all
-    redis.hkeys("algolia_tags").each do |key|
-      kind, name = key.split(/\:/)
-      count = redis.hget("algolia_tags", key).to_i
-      next if count < 10
-      name = name.squeeze(" ")
-      AlgoliaTag.create(name:name, kind:kind, count:count) if name.length < 80
-    end
-  end
 end
+
---
+++ lib/algolia/tagger.rb
@@ -0,0 +1,47 @@
+# -*- encoding : utf-8 -*-
+
+require 'rubygems'
+
+module AlgoliaFeed
+
+  class Tagger
+
+    TAGS_HASH = 'algolia_tags'
+
+    attr_accessor :redis
+
+    def initialize(redis=nil)
+      self.redis = redis || Redis.new
+    end
+
+    def self.build_from_redis(redis=nil)
+      self.new(redis).build_from_redis
+    end
+
+    def build_from_redis
+      AlgoliaTag.delete_all
+      self.redis.hkeys(TAGS_HASH).each do |key|
+        kind, name = key.split(/\:/)
+        count = self.redis.hget(TAGS_HASH, key).to_i
+        next if count < 10
+        name = name.squeeze(" ")
+        AlgoliaTag.create(name:name, kind:kind, count:count) if name.length < 80
+      end
+    end
+
+    def self.clear_redis(redis=nil)
+      self.new(redis).clear_redis
+    end
+
+    def clear_redis
+      self.redis.del(TAGS_HASH)
+    end
+
+    def increment(tag)
+      n = self.redis.hget(TAGS_HASH, tag).to_i
+      self.redis.hset(TAGS_HASH, tag, n + 1)
+    end
+
+  end
+end
+
