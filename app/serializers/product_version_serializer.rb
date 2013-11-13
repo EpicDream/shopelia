fe3aa297ab73eb49e13e9452ad06a7e405af1739
Eric Larchevêque <elarch@gmail.com>
2013-11-13 11:59:48 +0100

    Add rating in products serialization

--- app/serializers/product_version_serializer.rb
+++ app/serializers/product_version_serializer.rb
@@ -3,12 +3,16 @@ class ProductVersionSerializer < ActiveModel::Serializer
   
   attributes :id, :name, :description, :image_url, :price
   attributes :price_shipping, :price_strikeout, :shipping_info, :available
-  attributes :cashfront_value, :availability_info
+  attributes :cashfront_value, :availability_info, :rating
   attributes :option1, :option2, :option3, :option4
   attributes :option1_md5, :option2_md5, :option3_md5, :option4_md5
   
   def available
     object.available? ? 1 : 0
+  end
+
+  def rating
+    object.rating.to_f.round(2)
   end
 
   def cashfront_value
