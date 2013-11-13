e2da8c3d38a2b8f595a6eecd52b053bfb9edce2c
anoiaque <anoiaque@gmail.com>
2013-11-13 14:32:06 +0100

    * test on another sample

--- test/unit/parsers/descriptions/amazon/formatter_test.rb
+++ test/unit/parsers/descriptions/amazon/formatter_test.rb
@@ -3,6 +3,7 @@ require 'parsers/descriptions/amazon/formatter'
 
 class Descriptions::Amazon::FormatterTest < ActiveSupport::TestCase
   #sandisk : http://www.amazon.fr/SanDisk-SDMX18-004G-E46K-Sansa-Lecteur-MicroSD/dp/B002NX0MF0
+  #tigrou : http://www.amazon.fr/gp/product/B002MZZ2LI
   
   setup do
   end
@@ -13,14 +14,18 @@ class Descriptions::Amazon::FormatterTest < ActiveSupport::TestCase
     assert_equal Descriptions::Amazon::UlLiFormatter, formatter.node_formatter
   end
   
-  test "convert lis to array with lis text contents" do
-    formatter = formatter_for('sandisk_header')
+  test "convert lis to array with lis text contents for sandisk sample" do
+    representation = representation_for('sandisk_header')
     
-    hash = formatter.hash_representation
-    representation = hash[:summary]
-    
-    assert_equal 5, representation.count
-    assert_equal "Garantie du fabricant: 1 an", representation[0]
+    assert_equal 5, representation[:summary].count
+    assert_equal "Garantie du fabricant: 1 an", representation[:summary][0]
+  end
+  
+  test "convert lis to array with lis text contents for tigrou sample" do
+    representation = representation_for('tigrou_header')
+
+    assert_equal 5, representation[:summary].count
+    assert_equal "Age minimum: 2 ans", representation[:summary][0]
   end
   
   private
@@ -32,5 +37,9 @@ class Descriptions::Amazon::FormatterTest < ActiveSupport::TestCase
   def formatter_for sample
     html = description(sample)
     Descriptions::Amazon::Formatter.new(html)
+  end
+  
+  def representation_for sample
+    formatter_for(sample).hash_representation
   end
 end
