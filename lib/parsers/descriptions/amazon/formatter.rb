ecd89616b318a4e2d9b1a37b53f1c11431dab70e
anoiaque <anoiaque@gmail.com>
2013-11-13 17:03:41 +0100

    * TableFormatter * PFormatter

--- lib/parsers/descriptions/amazon/formatter.rb
+++ lib/parsers/descriptions/amazon/formatter.rb
@@ -3,17 +3,88 @@ module Descriptions
     
     class FormatterDetector
       def initialize nodeset
-        @nodeset = nodeset
+        @nodeset = nodeset.dup
       end
       
-      def formatter
+      def formatters
+        
+        table = @nodeset.xpath(".//table").first
+        return TableFormatter if table
+        
         ul = @nodeset.xpath(".//ul").first
-        UlLiFormatter if ul
+        return UlLiFormatter if ul
       end
     end
     
+    class PFormatter
+      
+      def initialize nodeset 
+        @nodeset = nodeset
+        @paragraphs = @nodeset.xpath(".//div/p")
+      end
+      
+      def representation
+        @paragraphs.inject({}) { |hash, paragraph|
+          key = header_of(paragraph)
+          hash[key] ||= []
+          hash[key] << paragraph.text
+          hash
+        }
+      end
+      
+      def header_of paragraph
+        xpath = (1..5).map { |n| 
+          "./#{paragraph.path.delete('?')}/preceding-sibling::h#{n}"
+        }.join(" | ")
+        node = @nodeset.xpath(xpath).first
+        node.text if node
+      end
+      
+    end
+    
+    class TableFormatter
+      DEFAULT_KEY = "Informations"
+      
+      attr_reader :key
+      
+      def initialize nodeset
+        @nodeset = nodeset
+        @headers = headers()
+        @tables = @nodeset.xpath(".//table")
+      end
+      
+      def representation
+        @tables.inject({}) { |hash, table|  
+          hash[@headers.shift] = table_to_keys_values(table)
+          hash
+        }
+      end
+      
+      def key
+        keys.first || DEFAULT_KEY
+      end
+      
+      def table_to_keys_values table
+        table.xpath(".//tr").inject({}) { |hash, tr|  
+          key, value = tr.xpath(".//td/text()").map(&:text)
+          next hash if key.blank? || value.blank?
+          hash[key] = value
+          hash
+        }
+      end
+      
+      def headers
+        @nodeset.xpath(".//div[@class='secHeader']/span").map(&:text) #specific
+      end
+      
+      def keys
+        @nodeset.xpath(".//*[self::h1 or self::h2 or self::h3 or self::h4]").map(&:text)
+      end
+      
+    end
+    
     class UlLiFormatter
-      DEFAULT_KEY = :summary
+      DEFAULT_KEY = "Résumé"
       
       def initialize nodeset
         @ul = nodeset.xpath(".//ul").first
--- test/unit/parsers/descriptions/amazon/formatter_test.rb
+++ test/unit/parsers/descriptions/amazon/formatter_test.rb
@@ -14,18 +14,47 @@ class Descriptions::Amazon::FormatterTest < ActiveSupport::TestCase
     assert_equal Descriptions::Amazon::UlLiFormatter, formatter.node_formatter
   end
   
+  test "formatter detector detect table type" do
+    formatter = formatter_for('sandisk_descriptif')
+    
+    assert_equal Descriptions::Amazon::TableFormatter, formatter.node_formatter
+  end
+  
   test "convert lis to array with lis text contents for sandisk sample" do
     representation = representation_for('sandisk_header')
     
-    assert_equal 5, representation[:summary].count
-    assert_equal "Garantie du fabricant: 1 an", representation[:summary][0]
+    assert_equal 5, representation["Résumé"].count
+    assert_equal "Garantie du fabricant: 1 an", representation["Résumé"][0]
   end
   
   test "convert lis to array with lis text contents for tigrou sample" do
     representation = representation_for('tigrou_header')
 
-    assert_equal 5, representation[:summary].count
-    assert_equal "Age minimum: 2 ans", representation[:summary][0]
+    assert_equal 5, representation["Résumé"].count
+    assert_equal "Age minimum: 2 ans", representation["Résumé"][0]
+  end
+  
+  test "convert simple tables to keys-values for sandisk sample" do
+    representation = representation_for('sandisk_descriptif')
+    puts representation.inspect
+    
+    descriptif = representation["Informations sur le produit"]["Descriptif technique"]
+    infos = representation["Informations sur le produit"]["Informations complémentaires"]
+    
+    assert_equal 11, descriptif.keys.count
+    assert_equal "SanDisk", descriptif["Marque"]
+    assert_equal "Garantie Fabricant : 1 an", descriptif["Garantie constructeur"]
+    
+    assert_equal 4, infos.keys.count
+    assert_equal "19 x 13 x 4 cm", infos["Dimensions du produit (L x l x h)"]
+  end
+  
+  test "something interesting" do
+    html = description('sandisk_descriptions')
+    @fragment = Nokogiri::HTML.fragment html
+    formatter = Descriptions::Amazon::PFormatter.new(@fragment)
+    
+    puts formatter.representation.inspect
   end
   
   private
