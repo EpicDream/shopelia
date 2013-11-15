7a7d55750fdd95cb592234a6f769b769133b6639
anoiaque <anoiaque@gmail.com>
2013-11-15 13:26:20 +0100

    * add process description to flow == Amazon only for now

--- lib/parsers/descriptions/formatter.rb
+++ lib/parsers/descriptions/formatter.rb
@@ -1,24 +1,23 @@
+require 'parsers/descriptions/amazon/formatter'
+
 module Descriptions
   module Formatter
     extend self
     
     #TODO temp, same formatter may be used for all merchants
     
-    def format product
-      return unless product.merchant.domain == "amazon.fr"
-      representation = Descriptions::Amazon::Formatter.format(product.description)
-      product.update_attributes(json_description: representation.to_json)
+    def format description, url
+      Descriptions::Amazon::Formatter.format(description).to_json
     rescue => e
-      report_incident_for product
+      puts e.backtrace.inspect
+      report_incident_for url
     end
     
-    def report_incident_for product
+    def report_incident_for url
       Incident.create(
         :issue => "Description Formatter",
         :severity => Incident::IMPORTANT,
-        :description => "product_id : #{product.id}",
-        :resource_type => 'Product',
-        :resource_id => @product.id)
+        :description => "#{url}")
     end
     
   end
