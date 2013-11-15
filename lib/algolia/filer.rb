17c91d4c04639307f394d3dff62d2e96e143b5f6
Manuel Valente <manuel.valente@gmail.com>
2013-11-15 17:27:03 +0100

    Fork issues solved

--- lib/algolia/filer.rb
+++ lib/algolia/filer.rb
@@ -160,48 +160,19 @@ module AlgoliaFeed
       end
     end
 
-    def fork_test
-      puts "[#{$$}] Master is starting."
-      c = 0
-      n = 10
-      pp = 0
-
-      while c < 100
-        pp = 1000
-        while true
-          px = Sys::ProcTable.ps.select{ |p| p.ppid == $$ && p.state != 'Z'}
-          break if px.size <= n
-          puts "Acrive children: #{px.size} - sleep 1"
-          sleep 1
-        end
-        c += 1
-        fork do
-          x = Random.rand(10)
-          puts "[#{$$}] Started - Sleeping for #{x}s"
-          sleep(x)
-          puts "[#{$$}] Done"
-          exit
-        end
-      end
-      Process.waitall
-    end
-
-    def process_xml_directory(dir=nil, free_children=6)
+    def process_xml_directory(dir=nil, children=6)
       algolia = AlgoliaFeed.new(self.params)
       algolia.connect(algolia.index_name)
       algolia.set_index_attributes
       Tagger.clear_redis
       dir = self.tmpdir unless dir.present?
-      trap('CLD') {
-        free_children += 1
-      }
       Find.find(dir) do |path|
         next unless File.file?(path)
-        while (free_children < 1)
+        while Sys::ProcTable.ps.select{ |p| p.ppid == $$ && p.state != 'Z'}.size >= children
           sleep 1
         end
-        free_children -= 1
         fork do
+          $0 = "Feed worker #{path}"
           ActiveRecord::Base.establish_connection
           class_name = path.split(/\//)[-2]
           worker = class_name.constantize.new(self.params)
