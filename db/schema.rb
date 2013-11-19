f13f6251ca980c17142aa3192487336ac20e20bb
anoiaque <anoiaque@gmail.com>
2013-11-19 11:57:08 +0100

    * Add json_description column to product_versions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131115191906) do
+ActiveRecord::Schema.define(:version => 20131119105432) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "user_id"
@@ -315,9 +315,9 @@ ActiveRecord::Schema.define(:version => 20131115191906) do
     t.integer  "merchant_id"
     t.string   "uuid"
     t.string   "state_name"
-    t.text     "message"
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
+    t.text     "message",                    :limit => 255
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
     t.string   "questions_json"
     t.string   "error_code"
     t.integer  "retry_count"
@@ -391,10 +391,10 @@ ActiveRecord::Schema.define(:version => 20131115191906) do
     t.float    "price_strikeout"
     t.string   "shipping_info"
     t.text     "description"
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
-    t.text     "option2"
-    t.text     "option1"
+    t.datetime "created_at",                       :null => false
+    t.datetime "updated_at",                       :null => false
+    t.text     "option2",           :limit => 255
+    t.text     "option1",           :limit => 255
     t.string   "name"
     t.boolean  "available"
     t.text     "image_url"
@@ -409,15 +409,16 @@ ActiveRecord::Schema.define(:version => 20131115191906) do
     t.string   "option3_md5"
     t.string   "option4_md5"
     t.float    "rating"
+    t.text     "json_description"
   end
 
   create_table "products", :force => true do |t|
     t.string   "name"
     t.integer  "merchant_id"
-    t.text     "url"
-    t.text     "image_url"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.text     "url",                 :limit => 255
+    t.text     "image_url",           :limit => 255
+    t.datetime "created_at",                                            :null => false
+    t.datetime "updated_at",                                            :null => false
     t.text     "description"
     t.integer  "product_master_id"
     t.string   "brand"
@@ -425,7 +426,7 @@ ActiveRecord::Schema.define(:version => 20131115191906) do
     t.boolean  "viking_failure"
     t.string   "reference"
     t.datetime "muted_until"
-    t.boolean  "options_completed",   :default => false
+    t.boolean  "options_completed",                  :default => false
     t.datetime "viking_sent_at"
     t.string   "image_size"
     t.float    "rating"
