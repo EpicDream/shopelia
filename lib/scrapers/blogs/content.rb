77d37d15716c41dd0d31905cfe5af98119b5199b
anoiaque <anoiaque@gmail.com>
2013-11-22 14:07:17 +0100

    * content text can be at node root

--- lib/scrapers/blogs/content.rb
+++ lib/scrapers/blogs/content.rb
@@ -8,7 +8,7 @@ module Scrapers
       
       def self.extract document
         content = document.search(".entry-content").first || document
-        texts = content.xpath(".//div/text()[normalize-space()] | .//p/text()[normalize-space()]").map(&:text)
+        texts = content.xpath(".//div/text()[normalize-space()] | .//p/text()[normalize-space()] | .//text()[normalize-space()]").map(&:text)
         texts.join("\n")
       end
       
