f4eb63c06424ed2c2cd1a7aef67a22a81c6d6a78
Eric Larchevêque <elarch@gmail.com>
2013-11-27 18:54:55 +0100

    * add model look_product

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131127171330) do
+ActiveRecord::Schema.define(:version => 20131127174413) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "user_id"
@@ -266,6 +266,13 @@ ActiveRecord::Schema.define(:version => 20131127171330) do
     t.integer  "resource_id"
     t.datetime "created_at",                       :null => false
     t.datetime "updated_at",                       :null => false
+  end
+
+  create_table "look_products", :force => true do |t|
+    t.integer  "look_id"
+    t.integer  "product_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "looks", :force => true do |t|
