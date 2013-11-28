821b554ef3af353cc60cf27d20c5ea4a80ab3321
anoiaque <anoiaque@gmail.com>
2013-11-28 14:36:08 +0100

    * new xpaths for search articles * add urls filters

--- test/integration/scrapers/blogs/blog_test.rb
+++ test/integration/scrapers/blogs/blog_test.rb
@@ -39,9 +39,11 @@ class Scrapers::Blogs::BlogTest < ActiveSupport::TestCase
 
   test "blog" do
     skip
-    @blog.url = "http://www.lauraoupas.com/"
+    @blog.url = "http://www.marieluvpink.com/"
     @blog.posts.each do |post|
       puts post.title
+      puts post.products.inspect
+      puts post.images.inspect
     end
   end
   
