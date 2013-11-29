1f62bd5b02ec781ef51b5010a5fbccf296bcbfb5
Vincent Renaudineau <vincent.renaudineau@prixing.fr>
2013-11-29 10:54:33 +0100

    [Viking] update UrbanoutfittersFr availabilities

--- test/unit/merchants/merchant_helper_test.rb
+++ test/unit/merchants/merchant_helper_test.rb
@@ -115,7 +115,7 @@ class MerchantHelperTest < ActiveSupport::TestCase
               "Epuisé", "pas disponible", "Currently unavailable., Currently unavailable.", "Rupture de stock",
               "Erreur: Désolé, mais le produit que vous avez demandé n'a pas été trouvé !",
               "La page que vous recherchez est introuvable.", "Ce produit n'existe plus ! Mais...",
-              "CE PRODUIT N’EST MALHEUREUSEMENT PLUS DISPONIBLE." ]
+              "CE PRODUIT N’EST MALHEUREUSEMENT PLUS DISPONIBLE.", "L'article ne peut pas être affiché." ]
     array.each do |str|
       assert_equal false, MerchantHelper.parse_availability(str)[:avail]
     end
--- test/unit/merchants/urbanoutfitters_fr_test.rb
+++ test/unit/merchants/urbanoutfitters_fr_test.rb
@@ -29,9 +29,9 @@ class UrbanoutfittersFrTest < ActiveSupport::TestCase
     @version = @helper.process_availability(@version)
     assert_equal text, @version[:availability_text]
 
-    # @version[:availability_text] = ""
-    # @version = @helper.process_availability(@version)
-    # assert_equal MerchantHelper::AVAILABLE, @version[:availability_text]
+    @version[:availability_text] = ""
+    @version = @helper.process_availability(@version)
+    assert_equal MerchantHelper::AVAILABLE, @version[:availability_text]
   end
 
   test "it should parse specific availability" do
