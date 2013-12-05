80bad2dfb91e1d815456a7885f73c710167e1163
anoiaque <anoiaque@gmail.com>
2013-12-05 12:56:00 +0100

    * 15mn css recreation

--- app/assets/javascripts/admin/blogs.js
+++ app/assets/javascripts/admin/blogs.js
@@ -3,12 +3,18 @@ $(document).ready(function() {
   $("#select-blog-url").on("change", function () {
     var blogId = $(this).val();
     window.location = "/admin/blogs/" + blogId;
-  })
+  });
   
   $(".fetch-post-link").on("click", function(event) {
     event.preventDefault();
-    $.get($(this)[0].href)
-    alert("Une tâche a été lançée pour scraper le blog...")
-  })
+    $.get($(this)[0].href);
+    alert("Une tâche a été lançée pour scraper le blog...");
+  });
+  
+  $("button[id^=integrate-blog]").on("click", function() {
+    var offset = $(this).offset();
+    $(".heart-overlay").toggleClass("heart-overlay-show");
+    $(".heart-overlay-show").offset({ top: offset.top, left: offset.left + 100 });
+  });
   
 });
