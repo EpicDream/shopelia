b50d982657668a6a88853b3a4e5be8eb68b32c84
anoiaque <anoiaque@gmail.com>
2013-12-05 13:43:19 +0100

    * fix

--- test/unit/blog_test.rb
+++ test/unit/blog_test.rb
@@ -1,9 +1,9 @@
-require 'test_helper'
+require 'test__helper'
 
 class BlogTest < ActiveSupport::TestCase
   
   test "create batch of blogs from csv file and return created blogs" do
-    csv = "http://www.leblogdebetty.com/,Betty\nhttp://www.adenorah.com/,Adenorah"
+    csv = "http://miss.com/,Betty\nhttp://www.adenorah.com/,Adenorah"
     blogs = []
     assert_difference("Blog.count", 2) do
       blogs = Blog.batch_create_from_csv(csv)
