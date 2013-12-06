71a6587fc0ac8d79f5ab93fbe61718d49ad71200
Vincent Renaudineau <vincent.renaudineau@prixing.fr>
2013-12-06 10:45:56 +0100

    [Viking] add NikeCom merchant helper

--- lib/merchants/merchant_helper.rb
+++ lib/merchants/merchant_helper.rb
@@ -217,7 +217,7 @@ class MerchantHelper
 
   #
   def process_image_url version
-    version[:image_url] = version[:image_url].sub(*@image_sub) if @image_sub && version[:image_url] && ! @config[:subImagesOnly]
+    version[:image_url] = version[:image_url].gsub(*@image_sub) if @image_sub && version[:image_url] && ! @config[:subImagesOnly]
     version
   end
 
@@ -225,7 +225,7 @@ class MerchantHelper
   def process_images version
     return version unless version[:images].present? && @image_sub && ! @config[:subImageUrlOnly]
     version[:images] = version[:images].map do |url|
-      url.sub(*@image_sub)
+      url.gsub(*@image_sub)
     end
     version
   end
