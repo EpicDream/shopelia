952ebcc242f9182cc11e44e842a34f4b850f4698
Shopelia <shopelia@shopelia.fr>
2013-12-06 12:05:21 +0100

    Fix

--- lib/merchants/publicidees_com.rb
+++ lib/merchants/publicidees_com.rb
@@ -6,7 +6,7 @@ class PublicideesCom
 
   def canonize
     if matches = /url=(.+)\Z/.match(@url)
-      url =  URI.unescape(matches[1])
+      url =  Utils.strip_tracking_params(URI.unescape(matches[1]))
       new_url = MerchantHelper.canonize(url)
       return new_url if new_url.present?
       return url unless MerchantHelper.is_aggregator?(url)
