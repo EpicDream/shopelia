584994c505e4cfaac4e07bff734275340c929b5e
Vincent Renaudineau <vincent.renaudineau@prixing.fr>
2013-12-09 16:48:09 +0100

    [Viking] refix in admin/viking

--- app/controllers/admin/viking_controller.rb
+++ app/controllers/admin/viking_controller.rb
@@ -6,7 +6,7 @@ class Admin::VikingController < Admin::AdminController
     @performance = @total > 0 ? result[false].to_f * 100 / (result[false].to_f + result[true].to_f) : 0
     
     merchants = {}
-    Product.where("products.updated_at > ?", 1.year.ago).joins(:merchant).group("merchants.name,products.viking_failure").select("count(*) as count,merchants.name,merchants.mapping_id,products.viking_failure").each do |s|
+    Product.where("products.updated_at > ?", 1.day.ago).joins(:merchant).group("merchants.name,products.viking_failure").select("count(*) as count,merchants.name,merchants.mapping_id,products.viking_failure").each do |s|
       merchants[s.name] ||= {}
       merchants[s.name][s.viking_failure] = s.count.to_f
       merchants[s.name][:viking_support] = s.mapping_id.present?
