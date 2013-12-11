7b0a4768dc397335a3753552155b2880232fc0a5
Eric Larchevêque <elarch@gmail.com>
2013-12-11 15:23:07 +0100

    * infinite registration for test@flinker.oi

--- app/models/flinker.rb
+++ app/models/flinker.rb
@@ -11,9 +11,19 @@ class Flinker < ActiveRecord::Base
   validates :email, :presence => true
   validates :username, length:{minimum:2}, allow_nil: true
   validates_confirmation_of :password
+  before_validation :reset_test_account
 
   has_attached_file :avatar, :url => "/images/flinker/:id/avatar.jpg", :path => "#{Rails.public_path}/images/flinker/:id/img.jpg"
 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username
   attr_accessible :name, :url, :is_publisher
+
+  private
+
+  def reset_test_account
+    if self.email.eql?("test@flink.io")
+      user = Flinker.find_by_email("test@flink.io")
+      user.destroy unless user.nil?
+    end
+  end
 end
