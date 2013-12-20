eb9c439f1e96511aaf270fe95c4f8a471781cc0a
Eric Larchevêque <elarch@gmail.com>
2013-12-20 12:48:06 +0100

    * flinkers index API

--- app/controllers/api/flink/flinkers_controller.rb
+++ app/controllers/api/flink/flinkers_controller.rb
@@ -1,1 +1,21 @@
-#create def_param_group
+class Api::Flink::FlinkersController < Api::Flink::BaseController
+  before_filter :retrieve_flinkers, :only => :index
+  before_filter :prepare_scope
+  
+  api :GET, "/flinkers", "Get flinkers"
+  def index
+    render json: {
+      flinkers: ActiveModel::ArraySerializer.new(@flinkers, scope:@scope)
+    }
+  end
+
+  private
+
+  def retrieve_flinkers
+    @flinkers = Flinker.where(is_publisher:true)
+  end
+
+  def prepare_scope
+    @scope = { developer:@developer, device:@device, flinker:current_flinker, short:true }
+  end
+end
