d6d7336d8dc774c8d86725dfa02fd174ebc50985
Eric Larchevêque <elarch@gmail.com>
2013-12-20 13:59:35 +0100

    * add descriptions to looks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131220105335) do
+ActiveRecord::Schema.define(:version => 20131220120237) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "user_id"
@@ -343,6 +343,7 @@ ActiveRecord::Schema.define(:version => 20131220105335) do
     t.datetime "created_at",                      :null => false
     t.datetime "updated_at",                      :null => false
     t.boolean  "is_published", :default => false
+    t.string   "description"
   end
 
   add_index "looks", ["is_published"], :name => "index_looks_on_is_published"
