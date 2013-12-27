b399b9bc083e44a2e19f932096c4b5fa7c8c0249
Eric Larchevêque <elarch@gmail.com>
2013-12-27 14:34:27 +0100

    * add codes/brand features to looks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131220120237) do
+ActiveRecord::Schema.define(:version => 20131227111637) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "user_id"
@@ -330,6 +330,8 @@ ActiveRecord::Schema.define(:version => 20131220120237) do
     t.integer  "product_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "code"
+    t.string   "brand"
   end
 
   add_index "look_products", ["look_id"], :name => "index_look_products_on_look_id"
@@ -446,9 +448,9 @@ ActiveRecord::Schema.define(:version => 20131220120237) do
     t.integer  "merchant_id"
     t.string   "uuid"
     t.string   "state_name"
-    t.text     "message"
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
+    t.text     "message",                    :limit => 255
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
     t.string   "questions_json"
     t.string   "error_code"
     t.integer  "retry_count"
@@ -551,10 +553,10 @@ ActiveRecord::Schema.define(:version => 20131220120237) do
     t.float    "price_strikeout"
     t.string   "shipping_info"
     t.text     "description"
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
-    t.text     "option2"
-    t.text     "option1"
+    t.datetime "created_at",                       :null => false
+    t.datetime "updated_at",                       :null => false
+    t.text     "option2",           :limit => 255
+    t.text     "option1",           :limit => 255
     t.string   "name"
     t.boolean  "available"
     t.text     "image_url"
@@ -577,10 +579,10 @@ ActiveRecord::Schema.define(:version => 20131220120237) do
   create_table "products", :force => true do |t|
     t.string   "name"
     t.integer  "merchant_id"
-    t.text     "url"
-    t.text     "image_url"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.text     "url",                 :limit => 255
+    t.text     "image_url",           :limit => 255
+    t.datetime "created_at",                                            :null => false
+    t.datetime "updated_at",                                            :null => false
     t.text     "description"
     t.integer  "product_master_id"
     t.string   "brand"
@@ -588,7 +590,7 @@ ActiveRecord::Schema.define(:version => 20131220120237) do
     t.boolean  "viking_failure"
     t.string   "reference"
     t.datetime "muted_until"
-    t.boolean  "options_completed",   :default => false
+    t.boolean  "options_completed",                  :default => false
     t.datetime "viking_sent_at"
     t.string   "image_size"
     t.float    "rating"
