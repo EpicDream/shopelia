1abc99629f98c944b1ee4d69a3e29cfffca0b6c0
anoiaque <anoiaque@gmail.com>
2013-12-31 14:56:15 +0100

    * follow users found by tag, and like one of their media * make waits points

--- lib/anne_fashion/instagram.rb
+++ lib/anne_fashion/instagram.rb
@@ -3,7 +3,8 @@ require 'instagram'
 module AnneFashion
   class Instagram
     CREDENTIALS = YAML.load_relative_file("accounts.yml")['instagram']
-    
+    MAX_FOLLOW_PER_SESSION = 20
+     
     attr_reader :client
     
     def initialize
@@ -11,11 +12,51 @@ module AnneFashion
       @client = ::Instagram::Client.new
     end
     
+    def follow_and_like_by_tag tag='fashion'
+      session_wait()
+      medias = search(tag).sample follow_count()
+      medias.each do |media|
+        next unless media.caption
+        begin
+          follow media.caption.from.id
+          wait(min=10)
+          like media.caption.id rescue("Cannot like")
+          wait
+        rescue
+          next
+        end
+      end
+    end
+    
     def me
       @client.user
     end
+    
+    def search tag='fashion', max=40
+      @client.tag_recent_media(tag, count:max)
+    end
+    
+    def follow user_id
+      @client.follow_user(user_id)
+    end
+    
+    def like media_id
+      @client.like_media(media_id)
+    end
 
     private
+    
+    def follow_count
+      rand(12..MAX_FOLLOW_PER_SESSION)
+    end
+    
+    def wait min=4
+      sleep rand(min..20)
+    end
+    
+    def session_wait
+      sleep rand(60..3600)
+    end
     
     def authenticate
       ::Instagram.configure do |config|
--- test/integration/anne_fashion/instagram_test.rb
+++ test/integration/anne_fashion/instagram_test.rb
@@ -4,12 +4,20 @@ require 'anne_fashion/instagram'
 
 class AnneFashion::InstagramTest < ActiveSupport::TestCase
   
+  #https://instagram.com/oauth/authorize/?client_id=845aac06f9bc40b69a521cef5611d7de&redirect_uri=http://localhost:3000&response_type=token&scope=likes+comments+relationships
+
   setup do
     @client = AnneFashion::Instagram.new
   end
   
   test "authentication" do
+    skip
     assert @client.me.username == 'huitrebzh'
+  end
+  
+  test "follow and like by tag" do
+    skip
+    @client.follow_and_like_by_tag('fashion')
   end
   
 end
