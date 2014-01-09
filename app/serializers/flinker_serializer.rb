aad7e0f59fde904a54d1d093c155c5f2bc60bc70
Eric Larchevêque <elarch@gmail.com>
2014-01-09 11:07:03 +0100

    * staff pick in serialization

--- app/serializers/flinker_serializer.rb
+++ app/serializers/flinker_serializer.rb
@@ -1,5 +1,9 @@
 class FlinkerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :url, :email, :username, :avatar, :country, :follows_count, :looks_count, :likes_count
+  attributes :id, :name, :url, :email, :username, :avatar, :country, :follows_count, :looks_count, :likes_count, :staff_pick
+
+  def staff_pick
+    object.staff_pick ? 1 : 0
+  end
 
   def name
     object.name.try(:strip)
