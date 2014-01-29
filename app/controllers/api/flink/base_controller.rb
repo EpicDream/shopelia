a407edb07ac9c866973c72f34415f5051cbc59d3
anoiaque <anoiaque@gmail.com>
2014-01-29 16:13:58 +0100

    [update] post comment if real user (that is not beta and dev flags on device)

--- app/controllers/api/flink/base_controller.rb
+++ app/controllers/api/flink/base_controller.rb
@@ -1,7 +1,7 @@
 class Api::Flink::BaseController < Api::ApiController
   skip_before_filter :authenticate_user!
   before_filter :authenticate_flinker!
-  before_filter :set_locate
+  before_filter :set_locale
   before_filter :set_navigator_properties
   before_filter :retrieve_device
 
