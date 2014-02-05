a9cc5055f9a7d35640f47126aa35bc54fb887056
anoiaque <anoiaque@gmail.com>
2014-02-05 17:16:04 +0100

    [temp] posts ordered by published at asc instead of followers count === Temp for one week

--- app/helpers/admin/posts_helper.rb
+++ app/helpers/admin/posts_helper.rb
@@ -2,7 +2,7 @@ module Admin::PostsHelper
   
   def look_preview_header post
     content_tag(:span, I18n.l(post.published_at), class:'look-published-at') +
-    content_tag(:span, "#{post.flinker_followers_count} followers", class:'look-published-at') +
+    #UNDO FOR ONE WEEK content_tag(:span, "#{post.flinker_followers_count} followers", class:'look-published-at') +
     button_tag(class:'btn btn-danger look-direct-reject', 'data-look-id' => post.look.id) {
       content_tag(:i, '', class:'icon-trash icon-white')
     }
