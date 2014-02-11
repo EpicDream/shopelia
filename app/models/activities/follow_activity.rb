d115ef719e16c2ce8efedd98527852e77000558f
anoiaque <anoiaque@gmail.com>
2014-02-11 18:13:55 +0100

    [new] send notification to flinker when he is followed + FlinkerFollow option to skip notification

--- app/models/activities/follow_activity.rb
+++ app/models/activities/follow_activity.rb
@@ -3,5 +3,6 @@ class FollowActivity < Activity
   
   def self.create! follow
     super(flinker_id:follow.flinker_id, target_id:follow.follow_id, resource_id:follow.id)
+    FollowNotificationWorker.perform_in(10.seconds, follow.follow_id, follow.flinker_id) unless follow.skip_notification
   end
 end
---
+++ test/unit/flink/activities/follow_activity_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class FollowActivityTest < ActiveSupport::TestCase
+  
+  setup do
+    @flinker = flinkers(:lilou)
+    @followed = flinkers(:betty)
+  end
+  
+  test "send follow notification to followed flinker when follow activity is created" do
+    Sidekiq::Testing.inline!
+    
+    Flink::FollowNotification.expects(:new).with(@followed, @flinker).returns(stub(:deliver => nil))
+    
+    FlinkerFollow.create(flinker_id:@flinker.id, follow_id:@followed.id)
+  end
+end
