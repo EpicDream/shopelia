dc89ce68ef3409a5c081d58f790e9ce10a40c28e
Shopelia <shopelia@shopelia.com>
2014-02-14 13:18:07 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -351,11 +351,11 @@ ActiveRecord::Schema.define(:version => 20140212105417) do
   add_index "flinkers", ["username"], :name => "index_flinkers_on_username"
 
   create_table "images", :force => true do |t|
-    t.text     "url",                  :limit => 1024
+    t.text     "url"
     t.string   "type"
-    t.datetime "created_at",                           :null => false
-    t.datetime "updated_at",                           :null => false
-    t.text     "picture_file_name",    :limit => 255
+    t.datetime "created_at",           :null => false
+    t.datetime "updated_at",           :null => false
+    t.text     "picture_file_name"
     t.string   "picture_content_type"
     t.integer  "picture_file_size"
     t.datetime "picture_updated_at"
@@ -524,9 +524,9 @@ ActiveRecord::Schema.define(:version => 20140212105417) do
     t.integer  "merchant_id"
     t.string   "uuid"
     t.string   "state_name"
-    t.text     "message",                    :limit => 255
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
+    t.text     "message"
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
     t.string   "questions_json"
     t.string   "error_code"
     t.integer  "retry_count"
@@ -628,10 +628,10 @@ ActiveRecord::Schema.define(:version => 20140212105417) do
     t.float    "price_strikeout"
     t.string   "shipping_info"
     t.text     "description"
-    t.datetime "created_at",                       :null => false
-    t.datetime "updated_at",                       :null => false
-    t.text     "option2",           :limit => 255
-    t.text     "option1",           :limit => 255
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
+    t.text     "option2"
+    t.text     "option1"
     t.string   "name"
     t.boolean  "available"
     t.text     "image_url"
@@ -654,10 +654,10 @@ ActiveRecord::Schema.define(:version => 20140212105417) do
   create_table "products", :force => true do |t|
     t.string   "name"
     t.integer  "merchant_id"
-    t.text     "url",                 :limit => 255
-    t.text     "image_url",           :limit => 255
-    t.datetime "created_at",                                            :null => false
-    t.datetime "updated_at",                                            :null => false
+    t.text     "url"
+    t.text     "image_url"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
     t.text     "description"
     t.integer  "product_master_id"
     t.string   "brand"
@@ -665,7 +665,7 @@ ActiveRecord::Schema.define(:version => 20140212105417) do
     t.boolean  "viking_failure"
     t.string   "reference"
     t.datetime "muted_until"
-    t.boolean  "options_completed",                  :default => false
+    t.boolean  "options_completed",   :default => false
     t.datetime "viking_sent_at"
     t.string   "image_size"
     t.float    "rating"
