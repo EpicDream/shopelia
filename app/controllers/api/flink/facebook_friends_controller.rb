8e296fc946fcafc9b55ba9f2af90935ce0730f0f
anoiaque <anoiaque@gmail.com>
2014-02-14 13:17:04 +0100

    * Fetch facebook friends if none at request * serialize fb flinkers friends as flinkers

--- app/controllers/api/flink/facebook_friends_controller.rb
+++ app/controllers/api/flink/facebook_friends_controller.rb
@@ -1,5 +1,6 @@
 class Api::Flink::FacebookFriendsController < Api::Flink::BaseController
   DEFAULT_PER_PAGE = 40
+  before_filter :fetch_facebook_friends, if: -> { FacebookFriend.of_flinker(current_flinker).limit(1).count.zero? }
   
   def index
     render json: { flinkers:flinkers, has_next:@has_next }
@@ -8,7 +9,7 @@ class Api::Flink::FacebookFriendsController < Api::Flink::BaseController
   private
   
   def flinkers
-    flink = paged FacebookFriend.of_flinker(current_flinker).flinkers
+    flink = paged(FacebookFriend.of_flinker(current_flinker).flinkers).map(&:friend)
     facebook = paged FacebookFriend.of_flinker(current_flinker).not_flinkers
     { facebook:ActiveModel::ArraySerializer.new(facebook), flink:ActiveModel::ArraySerializer.new(flink) }
   end
@@ -18,6 +19,10 @@ class Api::Flink::FacebookFriendsController < Api::Flink::BaseController
     res = collection.paginate(page:params[:page], per_page:per_page)
     @has_next = @has_next || res.total_pages > params[:page].to_i
     res
+  end
+  
+  def fetch_facebook_friends
+    FacebookFriend.create_or_update_friends(current_flinker)
   end
 
 end
