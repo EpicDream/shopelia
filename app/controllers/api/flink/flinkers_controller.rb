34df92fb5b1d461076f27a8cb4e6ea3554646b4d
anoiaque <anoiaque@gmail.com>
2014-02-17 14:22:18 +0100

    * API /flinkers now only for any flinker , filtered with username parameter

--- app/controllers/api/flink/flinkers_controller.rb
+++ app/controllers/api/flink/flinkers_controller.rb
@@ -1,6 +1,6 @@
 class Api::Flink::FlinkersController < Api::Flink::BaseController
   
-  api :GET, "/flinkers", "Get flinkers"
+  api :GET, "/flinkers", "Get flinkers filtered by username"
   def index
     render json: { flinkers: flinkers() }
   end
@@ -8,10 +8,8 @@ class Api::Flink::FlinkersController < Api::Flink::BaseController
   private
 
   def flinkers
-    query = params[:username] ? Flinker.with_username_like(params[:username]) : Flinker.publishers.with_looks
-    query = query.of_country(params[:country_iso]).includes(:country)
-    flinkers = query.paginate(pagination())
-    ActiveModel::ArraySerializer.new(flinkers)
+    flinkers = Flinker.with_username_like(params[:username]).paginate(pagination())
+    serialize flinkers
   end
 
 end
