ef3e67d58dadaebdc529cf0b38a57af2925f5723
anoiaque <anoiaque@gmail.com>
2014-02-26 13:48:36 +0100

    Sidekiq config

--- app/workers/follow_notification_worker.rb
+++ app/workers/follow_notification_worker.rb
@@ -2,6 +2,7 @@ require 'flink/notification'
 
 class FollowNotificationWorker
   include Sidekiq::Worker
+  sidekiq_options :queue => :notifications, retry:false
   
   def perform flinker_id, follower_id
     flinker, follower = [flinker_id, follower_id].map { |id| Flinker.find(id)}
