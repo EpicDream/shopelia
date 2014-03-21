f42b84118a4520b59717a3ef778059fccfd67c71
anoiaque <anoiaque@gmail.com>
2014-03-21 12:45:00 +0100

    * Statistics : Top liked and commented looks for yesterday

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -10,6 +10,23 @@ class Statistic
     ActiveRecord::Base.connection.execute(sql)
   end
   
+  def self.top_liked_looks_for_day date
+    FlinkerLike.where('created_at::DATE >= ? and created_at::DATE < ?', date, date + 1.day)
+    .where(resource_type:FlinkerLike::LOOK)
+    .group('resource_id')
+    .select('resource_id as look_id, count(*)')
+    .order('count desc')
+    .limit(5)
+  end
+
+  def self.top_commented_looks_for_day date
+    Comment.where('created_at::DATE >= ? and created_at::DATE < ?', date, date + 1.day)
+    .group('look_id')
+    .select('look_id, count(*)')
+    .order('count desc')
+    .limit(5)
+  end
+  
   private
   
   def to_ruby_date date
