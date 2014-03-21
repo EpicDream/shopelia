b68b27411a3a3e4485c599242424002c3ac5ec26
anoiaque <anoiaque@gmail.com>
2014-03-21 16:02:18 +0100

    * create directly new collection widget/ to edit * assign default cover * theme cover ; preserve_files

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -7,17 +7,27 @@ class Theme < ActiveRecord::Base
   has_and_belongs_to_many :hashtags
   has_one :theme_cover, foreign_key: :resource_id, dependent: :destroy
   
-  validates :title, presence:true
-  
   before_create :assign_default_rank
+  before_create :assign_default_cover, unless: -> { self.theme_cover }
+  before_validation :remove_blanks_hashtags
   
   accepts_nested_attributes_for :theme_cover
   accepts_nested_attributes_for :hashtags
   
   private
   
+  def remove_blanks_hashtags 
+    #to avoid validation error if hashtag blank, while not creating blank hashtags, another way ?
+    self.hashtags.delete_if { |hashtag| hashtag.name.blank?  }
+  end
+  
   def assign_default_rank
     self.rank = Theme.count + 1
+  end
+  
+  def assign_default_cover
+    self.theme_cover = ThemeCover.default.dup
+    self.theme_cover.save!
   end
   
 end
