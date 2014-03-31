bb624134ddc2eac7af880cca3b6a5f4f6ec2b34d
anoiaque <anoiaque@gmail.com>
2014-03-31 15:38:44 +0200

    * add country to theme serialized

--- test/unit/flink/serializers/theme_serializer_test.rb
+++ test/unit/flink/serializers/theme_serializer_test.rb
@@ -5,12 +5,18 @@ class ThemeSerializerTest < ActiveSupport::TestCase
   test "minimal serialization" do
     theme = themes(:mode)
     theme.send(:assign_default_cover)
-    
+    theme.countries << Country.first
+
     object = ThemeSerializer.new(theme).as_json[:theme]
 
-    assert_equal [:title, :subtitle, :position, :cover_height, :cover].to_set, object.keys.to_set
+    assert_equal [:title, :subtitle, :position, :cover_height, :cover, :country].to_set, object.keys.to_set
     assert_match "La mode c'est fun", object[:title]
     assert_match /http:\/\/www.flink.io\/images\/ae4\/large\/ae4fc89942443f7d5dda587fd1791ee7.jpg/, object[:cover]
+    assert_equal Country.first.iso, object[:country]
+  end
+  
+  test "maximal serialization, with hashtags, looks, flinkers" do
+    
   end
   
 end
