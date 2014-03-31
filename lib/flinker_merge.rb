5e7a5a7eefc20568deebeb78251bd3b7ad3a194d
anoiaque <anoiaque@gmail.com>
2014-03-31 18:16:31 +0200

    * flinker merge script #to test in rails console sandbox

---
+++ lib/flinker_merge.rb
@@ -0,0 +1,23 @@
+class FlinkerMerge
+  MAP_BACKUP_PATH = "/tmp/flinkers_merge.yml"
+  ATTRIBUTES = [:email, :country_id, :lang_iso, :encrypted_password, :authentication_token] #username ?
+  KLASSES = [FlinkerLike, Comment, FlinkerFollow, Device, Activity, FacebookFriend, FlinkerAuthentication]
+  
+  def initialize(flinker, target)
+    @flinker = flinker
+    @target = target
+    File.open(MAP_BACKUP_PATH, "a+") { |file| file.write("#{@flinker.id}: #{@target.id}\n")  }
+  end
+  
+  def merge
+    ActiveRecord::Base.transaction do
+      ATTRIBUTES.each {|attr| @target.send("#{attr}=", @flinker.send(attr)) }
+      KLASSES.each do |klass|
+        klass.where(flinker_id:@flinker.id).update_all(flinker_id:@target.id)
+      end
+      FlinkerFollow.where(follow_id:@flinker.id).update_all(follow_id:@target.id)
+      Activity.where(target_id:@flinker.id).update_all(target_id:@target.id)
+      FacebookFriend.where(friend_flinker_id:@flinker.id).update_all(friend_flinker_id:@target.id)
+    end
+  end
+end
