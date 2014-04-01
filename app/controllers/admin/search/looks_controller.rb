7c4cf6a538003ae6f418b9a7206f177d4644c765
anoiaque <anoiaque@gmail.com>
2014-04-01 17:32:38 +0200

    * default per page up

--- app/controllers/admin/search/looks_controller.rb
+++ app/controllers/admin/search/looks_controller.rb
@@ -2,7 +2,7 @@ class Admin::Search::LooksController < Admin::AdminController
   
   def index
     keywords = params[:keywords] ? params[:keywords].split(",") : []
-    @looks = Look.search(keywords).paginate(per_page:2, page:params[:page])
+    @looks = Look.search(keywords).paginate(per_page:30, page:params[:page])
   end
   
 end
