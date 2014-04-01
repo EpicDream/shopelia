dcd52d9a8f0704e47794cc3844e178fffe655ec6
anoiaque <anoiaque@gmail.com>
2014-04-01 11:41:23 +0200

    * add "--" to origin flinker email * unit tests on merge account

--- lib/flinker_merge.rb
+++ lib/flinker_merge.rb
@@ -1,6 +1,6 @@
 class FlinkerMerge
   MAP_BACKUP_PATH = "/tmp/flinkers_merge.yml"
-  ATTRIBUTES = [:email, :country_id, :lang_iso, :encrypted_password, :authentication_token] #username ?
+  ATTRIBUTES = [:email, :country_id, :lang_iso, :encrypted_password, :authentication_token]
   KLASSES = [FlinkerLike, Comment, FlinkerFollow, Device, Activity, FacebookFriend, FlinkerAuthentication]
   
   def initialize(flinker, target)
@@ -12,12 +12,18 @@ class FlinkerMerge
   def merge
     ActiveRecord::Base.transaction do
       ATTRIBUTES.each {|attr| @target.send("#{attr}=", @flinker.send(attr)) }
+      
       KLASSES.each do |klass|
         klass.where(flinker_id:@flinker.id).update_all(flinker_id:@target.id)
       end
+      
       FlinkerFollow.where(follow_id:@flinker.id).update_all(follow_id:@target.id)
       Activity.where(target_id:@flinker.id).update_all(target_id:@target.id)
       FacebookFriend.where(friend_flinker_id:@flinker.id).update_all(friend_flinker_id:@target.id)
+      
+      @flinker.update_attributes(email:@flinker.email + "--")
+      
+      @target.save!
     end
   end
 end
