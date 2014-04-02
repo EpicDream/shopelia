39af77cc65b4c9ecafafd0c8ad443a07b15d2000
anoiaque <anoiaque@gmail.com>
2014-04-02 14:23:29 +0200

    * Devise contact email * Mail preview => dev settings

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "Shopelia <contact@shopelia.com>"
+  config.mailer_sender = "Flink Fashion Link <hello@flink.io>"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
