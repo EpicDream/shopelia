402bdfe6d92dab950ae31e9799ec5c6e6cc4644e
anoiaque <anoiaque@gmail.com>
2014-04-09 18:07:16 +0200

    Look search by hashtag API point /hashtags/looks ; param => 'hashtag' : <String>

--- test/functional/api/flink/hashtags/looks_controller_test.rb
+++ test/functional/api/flink/hashtags/looks_controller_test.rb
@@ -6,18 +6,33 @@ class Api::Flink::Hashtags::LooksControllerTest < ActionController::TestCase
   setup do
     @flinker = flinkers(:fanny)
     sign_in @flinker
+    @hashtags = ["mode", "fashion", "tendance"].map { |name| Hashtag.create(name:name) }
+    looks(:agadir).hashtags << @hashtags.first
+    looks(:quimper).hashtags << @hashtags
   end
   
-  test "get looks with comments containing any given hashtags" do
-    Comment.update_all(body:"wha #beautiful !")
-    get :index, format: :json, hashtag:"#beautiful"
+  test "get looks with hashtags matching hashtag keyword" do
+    
+    get :index, format: :json, hashtag:"tendance"
     
     assert_response :success
     
     looks = json_response["looks"]
 
     assert_equal 1, looks.count
-    assert_equal flinkers(:betty).id, looks.first["flinker"]["id"]
+    assert_equal looks(:quimper).flinker.id, looks.first["flinker"]["id"]
+  end
+  
+  test "get looks with hashtags matching hashtag keyword, matching several looks" do
+    
+    get :index, format: :json, hashtag:"mode"
+    
+    assert_response :success
+    
+    looks = json_response["looks"]
+
+    assert_equal 2, looks.count
+    assert_equal [408576487, 520688968].to_set, looks.map { |look| look["flinker"]["id"] }.to_set
   end
   
 end
