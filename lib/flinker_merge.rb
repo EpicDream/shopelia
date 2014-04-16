047847d96c254b17f7de40d442aefa669d03fff4
anoiaque <anoiaque@gmail.com>
2014-04-16 17:34:20 +0200

    * merge flinkers from csv

--- lib/flinker_merge.rb
+++ lib/flinker_merge.rb
@@ -29,4 +29,30 @@ class FlinkerMerge
       @target.save!
     end
   end
+  
+  def self.merge_from_csv csv_path, col_sep=";"
+    CSV.foreach(csv_path, col_sep:col_sep) do |row|
+      blog_url, email = row[0].gsub!(/\/$/, '').strip, row[1].strip
+      
+      unless target = Flinker.where(url:url)
+        puts "Flinker with url #{url} not found, SKIPPED"
+        next
+      end
+      
+      unless Flinker.where(email:email).count == 1
+        puts "Many emails or not found : #{email}, SKIPPED"
+        next
+      end
+      
+      flinker = Flinker.where(email:email).first
+      
+      if flinker == target
+        puts "WARNING : Same flinker/target, SKIPPED"
+        next
+      end
+      
+      new(flinker, target).merge
+    end
+    
+  end
 end
