5c7534632d8785203acc3d05097045f96751a0e1
anoiaque <anoiaque@gmail.com>
2014-04-17 16:02:40 +0200

    * remove rank order, seems something is wrong somewhere

--- app/controllers/admin/themes_previews_controller.rb
+++ app/controllers/admin/themes_previews_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ThemesPreviewsController < Admin::AdminController
   
   def show
-    @themes = Theme.pre_published_or_published.order('rank asc')
+    @themes = Theme.pre_published_or_published
   end
 end
