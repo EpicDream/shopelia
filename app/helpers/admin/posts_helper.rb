047056eaa78d9790a025963554616e03ca5a852b
anoiaque <anoiaque@gmail.com>
2014-04-23 11:37:00 +0200

    * add publisher username

--- app/helpers/admin/posts_helper.rb
+++ app/helpers/admin/posts_helper.rb
@@ -1,6 +1,7 @@
 module Admin::PostsHelper
   
   def look_preview_header post
+    content_tag(:span, post.look.flinker.username, class:'look-username') +
     content_tag(:span, I18n.l(post.published_at), class:'look-published-at') +
     button_tag(class:'btn btn-danger look-direct-reject', 'data-look-id' => post.look.id) {
       content_tag(:i, '', class:'icon-trash icon-white')
