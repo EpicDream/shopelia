370a18167fb470e7515f700b87becde6c21091d8
anoiaque <anoiaque@gmail.com>
2014-04-23 11:16:43 +0200

    * Use server image url instead of blog one * Cosmetic

--- app/helpers/admin/posts_helper.rb
+++ app/helpers/admin/posts_helper.rb
@@ -2,7 +2,6 @@ module Admin::PostsHelper
   
   def look_preview_header post
     content_tag(:span, I18n.l(post.published_at), class:'look-published-at') +
-    #UNDO FOR ONE WEEK content_tag(:span, "#{post.flinker_followers_count} followers", class:'look-published-at') +
     button_tag(class:'btn btn-danger look-direct-reject', 'data-look-id' => post.look.id) {
       content_tag(:i, '', class:'icon-trash icon-white')
     }
@@ -10,7 +9,7 @@ module Admin::PostsHelper
   
   def look_preview post
     link_to content_tag(:div) { 
-      image_tag(post.images.first) + 
+      image_tag(post.look.look_images.first.picture(:large)) + 
       content_tag(:p, post.title, class:"post-title")
     }, admin_post_path(post)
   end
