da48a2969695d8cb9dbffed336cb40a12cc934b4
anoiaque <anoiaque@gmail.com>
2014-04-29 16:39:49 +0200

    * clean

--- app/models/flinker_like.rb
+++ app/models/flinker_like.rb
@@ -12,16 +12,6 @@ class FlinkerLike < ActiveRecord::Base
   validates :flinker_id, :presence => true
   validates :resource_type, :presence => true, :inclusion => { :in => [ PRODUCT, LOOK ] }
   validates :resource_id, :presence => true, :uniqueness => { :scope => [:flinker_id, :resource_type]}
-
-  scope :top_likers, ->(max=20, since=Date.parse("2014-01-01")) { 
-    where('created_at::DATE >= ?', since)
-    .where(resource_type:LOOK)
-    .includes(:flinker)
-    .group('flinker_id')
-    .select('flinker_id, count(*)')
-    .limit(max)
-    .order('count(*) desc')
-  }
   
   scope :likes_for, ->(flinkers, type=LOOK) {
     flinkers = [flinkers].flatten
