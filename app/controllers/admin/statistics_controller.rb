f76da78f05369b4326b9d96480add989eb96c29f
anoiaque <anoiaque@gmail.com>
2014-04-29 12:14:34 +0200

    * Most liked  looks last week

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -3,6 +3,7 @@ class Admin::StatisticsController < Admin::AdminController
   def index
     @statistics = Statistic.new.of_publishers
     @statistics_for_day = Statistic.new(from:params[:from_date]).of_publishers
+    @likes = Statistic.top_liked_looks_for_day(Time.now - 1.week, Time.now, 20)
   end
   
 end
