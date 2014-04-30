ca0f2d5fa06cbd36962aa2108ff4dd3f10c0d582
anoiaque <anoiaque@gmail.com>
2014-04-30 10:43:26 +0200

    * cache flinkers friend suggestions 24 hours

--- app/controllers/api/flink/top_flinkers_controller.rb
+++ app/controllers/api/flink/top_flinkers_controller.rb
@@ -7,8 +7,10 @@ class Api::Flink::TopFlinkersController < Api::Flink::BaseController
   private
   
   def flinkers
-    flinkers = Flinker.similar_to(current_flinker)
-    ActiveModel::ArraySerializer.new(flinkers)
+    Rails.cache.fetch([:top_flinkers_controller, :index, current_flinker.id], expires_in: 24.hours) do
+      flinkers = Flinker.similar_to(current_flinker)
+      ActiveModel::ArraySerializer.new(flinkers)
+    end
   end
   
 end
