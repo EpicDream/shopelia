4dbd4d00ed6ace8aedaac12abf5deea0a934a334
anoiaque <anoiaque@gmail.com>
2014-05-05 16:29:38 +0200

    * cut

--- app/controllers/api/flink/looks_controller.rb
+++ app/controllers/api/flink/looks_controller.rb
@@ -22,10 +22,11 @@ class Api::Flink::LooksController < Api::Flink::BaseController
       flinker = Flinker.where(id:params[:flinker_id]).first || current_flinker
       Look.liked_by(flinker).order(LOOKS_ORDER).paginate(pagination)
     when params[:updated_after] #CHANGED: => /flink/followings/updated_looks
-      Look.of_flinker_followings(current_flinker)
-      .updated_after(params[:updated_after])
-      .order('updated_at asc')
-      .paginate(pagination)
+      []
+      # Look.of_flinker_followings(current_flinker)
+      # .updated_after(params[:updated_after])
+      # .order('updated_at asc')
+      # .paginate(pagination)
     when params[:flinker_ids] #CHANGED: => /flink/flinkers/looks
       Look.where(flinker_id:params[:flinker_ids])
       .published_between(params[:published_after], params[:published_before])
