c7c189d8c8e8ff89c902e7302cd6ea9ff87425f2
anoiaque <anoiaque@gmail.com>
2014-05-09 15:57:19 +0200

    * fix : regexp too long

--- app/helpers/admin/looks_helper.rb
+++ app/helpers/admin/looks_helper.rb
@@ -1,10 +1,13 @@
 module Admin::LooksHelper
   def highlighted_brands html, brands
-    regexp = Regexp.new(brands.join("|"), true)
-    matches = html.scan(regexp).uniq
-    matches.each do |brand|
-      html.gsub!(/#{brand}/i, "<span class='highlight'>#{brand}</span>")
+    matches = []
+    brands.each_slice(100) do |slice|
+      regexp = Regexp.new(slice.join("|"), true)
+      matches += html.scan(regexp)
+    end
+    matches.flatten.uniq.compact.each do |brand|
+      html.gsub!(/\W#{brand}\W/i, "<span class='highlight'>#{brand}</span>")
     end
     html
-  end
+end
 end
