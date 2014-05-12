0c766401f624a979faf67219c7139afd4a54adbf
anoiaque <anoiaque@gmail.com>
2014-05-12 15:31:36 +0200

    * Enhance brands highlight

--- app/helpers/admin/looks_helper.rb
+++ app/helpers/admin/looks_helper.rb
@@ -1,12 +1,13 @@
 module Admin::LooksHelper
   def highlighted_brands html, brands
+    html = strip_tags(html)
     matches = []
     brands.each_slice(100) do |slice|
       regexp = Regexp.new(slice.join("|"), true)
       matches += html.scan(regexp)
     end
     matches.flatten.uniq.compact.each do |brand|
-      html.gsub!(/\W#{brand}\W/i, "<span class='highlight'>#{brand}</span>")
+      html.gsub!(/(\W|^)#{brand}(\W|$)/i, "<span class='highlight'>#{brand}</span>")
     end
     html
 end
