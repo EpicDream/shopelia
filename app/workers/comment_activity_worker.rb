6edcb726eb252516f5b9d9ee5fe66041a8d8ecec
anoiaque <anoiaque@gmail.com>
2014-05-12 13:10:13 +0200

    * Create Comment activities in background.

---
+++ app/workers/comment_activity_worker.rb
@@ -0,0 +1,11 @@
+class CommentActivityWorker
+  include Sidekiq::Worker
+  
+  def perform comment_id
+    comment = Comment.find(comment_id)
+    comment.flinker.followers.each do |friend|
+      next if CommentTimelineActivity.for_comment_and_target(comment, friend).first
+      CommentActivity.create(flinker_id:comment.flinker_id, target_id:friend.id, resource_id:comment.id)
+    end
+  end
+end
