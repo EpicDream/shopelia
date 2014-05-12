2e7006b5b483e14d29ac6a86efc803d54032a7a0
anoiaque <anoiaque@gmail.com>
2014-05-12 15:02:07 +0200

    * do not create share activity if flinker device build <= 26

--- app/models/activities/share_activity.rb
+++ app/models/activities/share_activity.rb
@@ -1,5 +1,6 @@
 class ShareActivity < Activity
   ONLY = [SocialNetwork::FACEBOOK, SocialNetwork::TWITTER]
+  MIN_BUILD_NUMBER = 26
   
   belongs_to :resource, foreign_key: :resource_id, class_name:'LookSharing'
   
@@ -8,7 +9,9 @@ class ShareActivity < Activity
     # return unless ONLY.include?(look_sharing.social_network.name)
     # flinker = look_sharing.flinker
     # flinker.followers.each do |friend|
-    #   super(flinker_id:flinker.id, target_id:friend.id, resource_id:look_sharing.id)
+    #   if friend.device.try(:build) && friend.device.build > MIN_BUILD_NUMBER
+    #     super(flinker_id:flinker.id, target_id:friend.id, resource_id:look_sharing.id)
+    #   end
     # end
   end
   
