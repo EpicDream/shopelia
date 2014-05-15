b2602c9abb4388b9f22c02d86435dafa9a4d7d70
anoiaque <anoiaque@gmail.com>
2014-05-15 15:03:10 +0200

    * send email to staff when flinkHQ is mentionned

--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -1,8 +1,17 @@
 class Emailer < ActionMailer::Base
+  
   def password_reset flinker
     @resource = flinker
     mail(:to => @email,
   		   :subject => "Reset Password",
   	     :from => "Flink Fashion Link <hello@flink.io>")
   end
+  
+  def flinkhq_mention comment
+    @comment = comment
+    mail(:to => 'staff@flink.io',
+  		   :subject => 'flinkHQ mentionné dans un commentaire',
+  	     :from => 'The genius at backend <genius@flink.io>')
+  end
+  
 end
