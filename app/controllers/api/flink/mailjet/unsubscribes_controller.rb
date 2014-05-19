032fa57ee6ee271eadfc2ddebd8d94077eb0a6d6
anoiaque <anoiaque@gmail.com>
2014-05-19 12:27:02 +0200

    * set flinker newsletter flag to false when mailjet event unsubscribe

--- app/controllers/api/flink/mailjet/unsubscribes_controller.rb
+++ app/controllers/api/flink/mailjet/unsubscribes_controller.rb
@@ -4,7 +4,10 @@ class Api::Flink::Mailjet::UnsubscribesController < Api::Flink::BaseController
   skip_before_filter :authenticate_developer!
   
   def create
-    File.open("/tmp/mailjet-call.log", "a+") { |f| f.write("\n[MAILJET]#{params}\n") }
+    if params[:unsubscribe] && flinker = Flinker.where(email:params[:unsubscribe][:email]).first
+      flinker.update_attributes(newsletter:false)
+    end
+    
     render json:{}, status:200
   end
 end
