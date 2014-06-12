f2de2bfeff0e050cc7845998ef99103f6b324623
anoiaque <anoiaque@gmail.com>
2014-06-12 17:00:30 +0200

    * Trends setters offline mode * handle param country iso

--- app/controllers/api/flink/trend_setters_controller.rb
+++ app/controllers/api/flink/trend_setters_controller.rb
@@ -1,14 +1,15 @@
 class Api::Flink::TrendSettersController < Api::Flink::BaseController
+  skip_before_filter :authenticate_flinker!
   
   def index
-    render unauthorized and return unless current_flinker
     render json: { flinkers: flinkers }
   end
   
   private
   
   def flinkers
-    Flinker.trend_setters(current_flinker.country)
+    country = current_flinker ? current_flinker.country : Country.where(iso:params[:iso]).first
+    Flinker.trend_setters(country)
   end
   
 end
