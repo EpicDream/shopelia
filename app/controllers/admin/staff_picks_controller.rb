be2b794809d9e7b85f4f24d73045c360bfbd9fd2
anoiaque <anoiaque@gmail.com>
2014-06-16 12:44:59 +0200

    * ADMIN : highlighted hashtags + counts for each section

--- app/controllers/admin/staff_picks_controller.rb
+++ app/controllers/admin/staff_picks_controller.rb
@@ -3,6 +3,7 @@ class Admin::StaffPicksController < Admin::AdminController
   def index
     @looks = Look.where(staff_pick:true)
     @flinkers = Flinker.staff_pick
+    @hashtags = Hashtag.highlighted.includes(:looks)
   end
   
 end
