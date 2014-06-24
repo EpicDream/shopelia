4812ab6beea080d6576efbc8b5f98a5322d674ed
anoiaque <anoiaque@gmail.com>
2014-06-24 17:24:28 +0200

    * Reject looks on posts view by batch via checkbox checking

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -4,7 +4,6 @@ class Admin::PostsController < Admin::AdminController
   def index
     @posts = Post.pending_processing.of_country(params[:country_code]).of_blog_with_name(params[:blog_name])
     .order('published_at asc')
-    #UNDO FOR ONE WEEK .with_followers_count
     @publications = Look.publications_counts_per_day
   end
   
