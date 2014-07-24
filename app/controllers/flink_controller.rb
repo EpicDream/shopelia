73ed5524ac2e43e26149c0faf961f4184087feed
anoiaque <anoiaque@gmail.com>
2014-07-24 18:18:00 +0200

    * NEW layout for FlinkController * NEW CSS and JS assets * Begin integrate metas for SEO/Robots

--- app/controllers/flink_controller.rb
+++ app/controllers/flink_controller.rb
@@ -1,19 +1,7 @@
 class FlinkController < ApplicationController
-  layout "flink"
+  layout "flink-fashion"
 
   def index
   end
 
-  def terms
-
-  end
-
-  def download
-    user_agent = request.env['HTTP_USER_AGENT'].downcase
-    if user_agent.match(/iphone/) || user_agent.match(/ipod/) || user_agent.match(/ipad/)
-      redirect_to "https://itunes.apple.com/fr/app/shopelia-lachat-facile-!/id731471392?mt=8"
-    else
-      redirect_to "/"
-    end
-  end
 end
