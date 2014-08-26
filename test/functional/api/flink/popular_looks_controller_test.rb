54b6932b40f70494874feb9e79dd24f92d5b90a6
anoiaque <anoiaque@gmail.com>
2014-08-26 15:02:45 +0200

    * Update tests

--- test/functional/api/flink/popular_looks_controller_test.rb
+++ test/functional/api/flink/popular_looks_controller_test.rb
@@ -7,6 +7,7 @@ class Api::Flink::PopularLooksControllerTest < ActionController::TestCase
     @flinker = flinkers(:betty)
     sign_in @flinker
     Look.all.each { |look| look.update_attributes(is_published:true) }
+    Look.send(:remove_const, :MIN_LIKES_FOR_POPULAR)
     Look.const_set(:MIN_LIKES_FOR_POPULAR, 1)
   end
 
