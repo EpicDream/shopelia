60aa44d6b20123a594d7ec704ff74bda1dc18628
Nicolas Bigot <nicolasbigot@me.com>
2014-09-01 17:21:20 +0200

    Add grid home on iphone

--- app/controllers/flink/explore_controller.rb
+++ app/controllers/flink/explore_controller.rb
@@ -3,9 +3,17 @@ class Flink::ExploreController < ApplicationController
   layout "flink"
 
   def show
-    @category = category
-    @covers = Look.covers.includes(:hashtags).send(@category).paginate(per_page: 20, page: params[:page])
-    render partial: 'covers' if request.xhr?
+    if is_mobile_device? && !request.xhr?
+      @covers_flink_loves = Look.covers.includes(:hashtags).send(:flink_loves).paginate(per_page: 20, page: params[:page])
+      @covers_popular = Look.covers.includes(:hashtags).send(:popular).paginate(per_page: 20, page: params[:page])
+      @covers_recent = Look.covers.includes(:hashtags).send(:recent).paginate(per_page: 20, page: params[:page])
+      @covers = (@covers_flink_loves + @covers_popular + @covers_recent).uniq
+    else
+      @category = category
+      @covers = Look.covers.includes(:hashtags).send(@category).paginate(per_page: 20, page: params[:page])
+      render partial: 'covers.mobile.html.erb', :locals => {:covers => @covers} if request.xhr? && is_mobile_device?
+      render partial: 'covers.html.erb' if request.xhr? && !is_mobile_device?
+    end
   end
   
   private
