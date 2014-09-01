ac5a71606083f46c37ce4b223e1436748152dc73
anoiaque <anoiaque@gmail.com>
2014-09-01 15:59:26 +0200

    [API] LookProductSerialize : add vendor product id

--- app/serializers/look_product_serializer.rb
+++ app/serializers/look_product_serializer.rb
@@ -5,12 +5,12 @@ class LookProductSerializer < ActiveModel::Serializer
     return "" if object.code.blank?
     I18n.t("flink.products." + object.code, raise: true)
   rescue I18n::MissingTranslationData
-    nil.to_s #:) 2 bytes saved
+    nil.to_s
   end
   
   def products
     object.vendor_products.map { |product|
-      { url: product.url, similar: product.similar }
+      { url: product.url, similar: product.similar, id: product.id }
     }
   end
 end
