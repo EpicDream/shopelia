d9cf53b8c3471255d47ae11c2aa1e237d0aeb263
anoiaque <anoiaque@gmail.com>
2014-09-04 15:40:34 +0200

    * Enable metadata to APNS push notif

--- lib/flink/push.rb
+++ lib/flink/push.rb
@@ -1,12 +1,18 @@
 module Flink
   module Push
 
-    def self.deliver message, device
+    def self.deliver message, device, metadata={}
       return if device.nil? || device.push_token.nil?
       config = Rails.application.config.apns[device.env]
       [:host, :port, :pem, :pass].each { |key| APNS.send("#{key}=", config[key]) }
-      APNS.send_notification(device.push_token, alert:message.first(150), :"content-available" => 1, :sound => 'default')
       
+      APNS.send_notification(
+        device.push_token, 
+        alert: message.first(150), 
+        :"content-available" => 1, 
+        sound: 'default', 
+        other: { metadata: metadata }
+      )
     end
 
   end
