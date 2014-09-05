c5be768ab46b00c34553109b5482e9fb0350bd9c
anoiaque <anoiaque@gmail.com>
2014-09-05 12:27:46 +0200

    * APNS notifications with targets (Flinker, Theme, Hashtag, and Look)

--- app/models/apns_notification.rb
+++ app/models/apns_notification.rb
@@ -1,18 +1,37 @@
 require 'flink/push'
 
 class ApnsNotification < ActiveRecord::Base
-  EMAILS_FOR_TESTING = ["olivierfisch@hotmail.com", "anoiaque@gmail.com"]
-  attr_accessible *column_names
+  TARGETS = ["Look", "Flinker", "Hashtag", "Theme"]
   
   def apns_test
-    devices = Device.where(flinker_id: Flinker.where(email:EMAILS_FOR_TESTING).map(&:id))
-    Flink::Push.deliver_by_batch(text_fr, devices)
-    Flink::Push.deliver_by_batch(text_en, devices)
+    devices = Device.where(flinker_id: Flinker.where(email: Rails.configuration.emails_for_testing).map(&:id))
+    Flink::Push.deliver_by_batch(text_fr, devices, metadata)
+    Flink::Push.deliver_by_batch(text_en, devices, metadata)
   end
   
   def send_to_all_flinkers
-    ApnsNotificationWorker.perform_async(text_fr, :fr)
-    ApnsNotificationWorker.perform_async(text_en, :en)
+    ApnsNotificationWorker.perform_async(text_fr, :fr, metadata)
+    ApnsNotificationWorker.perform_async(text_en, :en, metadata)
+  end
+  
+  private
+  
+  def metadata
+    return {} if self.resource_klass_name.nil?
+    { "link_kind" => link_kind , "identifier" => identifier }
+  end
+  
+  def link_kind
+    "#{self.resource_klass_name.downcase}s"
+  end
+  
+  def identifier
+    case self.resource_klass_name
+    when "Look" then Look.find(self.resource_id).uuid
+    when "Flinker" then self.resource_id.to_s
+    when "Hashtag" then Hashtag.find(self.resource_id).name
+    when "Theme" then nil
+    end
   end
   
 end
---
+++ db/migrate/20140905091721_add_apns_notifications_targets_of_message.rb
@@ -0,0 +1,6 @@
+class AddApnsNotificationsTargetsOfMessage < ActiveRecord::Migration
+  def change
+    add_column :apns_notifications, :resource_id, :integer
+    add_column :apns_notifications, :resource_klass_name, :string, default:nil
+  end
+end
