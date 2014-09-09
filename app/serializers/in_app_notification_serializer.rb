7a7a025014a1b568af231b507edefcc7cead2da8
anoiaque <anoiaque@gmail.com>
2014-09-09 18:10:42 +0200

    * image host

--- app/serializers/in_app_notification_serializer.rb
+++ app/serializers/in_app_notification_serializer.rb
@@ -2,6 +2,6 @@ class InAppNotificationSerializer < ActiveModel::Serializer
   attributes :id, :title, :subtitle, :content, :button_title, :image_url
   
   def image_url
-    object.image.picture.url
+    Rails.configuration.image_host + object.image.picture.url
   end
 end
