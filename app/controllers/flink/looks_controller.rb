6ae480f6b1bcd863083b6770f23709ceb6685f41
Nicolas Bigot <nicolasbigot@icloud.com>
2014-09-10 15:59:40 +0200

    Add monetized product links

--- app/controllers/flink/looks_controller.rb
+++ app/controllers/flink/looks_controller.rb
@@ -4,6 +4,45 @@ class Flink::LooksController < ApplicationController
   def show
     @look = Look.with_uuid(params[:id]).first
     @look ||= Look.find(params[:id])
+
+    @matched_products = []
+    @similar_products = []
+    at_least_one_product_is_purchasable = products_have_at_least_one_purchasable? @look.look_products
+    for product in @look.look_products do
+      hash = hash_from_product(product, at_least_one_product_is_purchasable)
+      if product_is_similar? product then
+        @similar_products << hash
+      else
+        @matched_products << hash
+      end
+    end
   end
 
+  private
+  def hash_from_product product, render_shop_badge
+    { code: product.code, brand: product.brand, monetized_url: product_monetized_url(product), is_hot: product_is_hot?(product), render_shop_badge: render_shop_badge }
+  end
+
+  def product_is_similar? product
+    product.vendor_products.first && product.vendor_products.first.similar
+  end
+
+  def product_is_purchasable? product
+    product.vendor_products.first && product.vendor_products.first.url && product.vendor_products.first.url.length > 0
+  end
+
+  def product_is_hot? product
+    product.vendor_products.first && product.vendor_products.first.staff_pick
+  end
+
+  def product_monetized_url product
+    product_is_purchasable?(product) ? product.vendor_products.first.url : ""
+  end
+
+  def products_have_at_least_one_purchasable? products
+    for product in products
+      return true if product_is_purchasable? product
+    end
+    return false
+  end
 end
