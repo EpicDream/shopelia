be33b9aa3035528f5234ab4dffef7b4d70c50372
Nicolas Bigot <nicolasbigot@icloud.com>
2014-09-11 17:06:59 +0200

    Add TripeLift ads to mobile grids

--- app/assets/javascripts/flink/explore.mobile.js
+++ app/assets/javascripts/flink/explore.mobile.js
@@ -6,21 +6,34 @@ $(document).ready(function() {
 
         var isRunning = false;
         var currentPage = 2;
+        var carousel = viewport.children('.covers-carousel');
+        var category = carousel.data('category');
+
+        // observe scroll
         viewport.bind('scroll', function() {
 
             if (isRunning) return ;
-            var carousel = viewport.children('.covers-carousel');
-            var category = carousel.data('category');
             if (viewport.scrollLeft() + viewport.width() >= carousel.width() * 0.8) {
 
-                // ajax call
+                 // ajax call
                 isRunning = true;
                 $.get('/explore/' + category + '?page=' + currentPage)
                     .done(function(covers) {
                         var beforeCount = carousel.children('.cover').length;
+
+                        // append ad
+                        var div = document.createElement("div");
+                        div.className = "ad";
+                        var script = document.createElement("script");
+                        script.type = "text/javascript";
+                        script.src = "http://ib.3lift.com/ttj?inv_code=flink_main_feed";
+                        div.insertBefore(script, null);
+                        carousel.get(0).insertBefore(div, null);
+
+                        // append covers
                         carousel.append(covers);
+
                         var afterCount = carousel.children('.cover').length;
-                        carousel.width(110 * afterCount - 10);
                         currentPage++;
                         if (afterCount - beforeCount < 20)
                             viewport.unbind('scroll');
