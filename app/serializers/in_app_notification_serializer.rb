f0d23fc670511daa1b0e948ae3305359aff8bcec
anoiaque <anoiaque@gmail.com>
2014-09-12 10:48:57 +0200

    [API] Add link_kind and link_identifier for a in app notification

--- app/serializers/in_app_notification_serializer.rb
+++ app/serializers/in_app_notification_serializer.rb
@@ -1,7 +1,16 @@
 class InAppNotificationSerializer < ActiveModel::Serializer
-  attributes :id, :title, :subtitle, :content, :button_title, :image_url
+  attributes :id, :title, :subtitle, :content, :button_title
+  attributes :link_kind, :link_identifier, :image_url
   
   def image_url
     Rails.configuration.image_host + object.image.picture.url
+  end
+  
+  def link_kind
+    object.resource_klass_name
+  end
+  
+  def link_identifier
+    object.resource_id
   end
 end
