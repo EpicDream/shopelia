30d7572870151456c4285dbe3e3da49470f37001
anoiaque <anoiaque@gmail.com>
2014-09-18 14:49:21 +0200

    * remove unused for flink

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,23 +6,7 @@ class HomeController < ApplicationController
       sign_out(current_flinker) if current_flinker
       render 'after_signin', layout:'flink'
     else
-      logger.error request.original_url
       redirect_to :root
     end
-  end
-
-  def download
-    user_agent = request.env['HTTP_USER_AGENT'].downcase
-    if user_agent.match(/android/)
-      redirect_to "market://details?id=com.shopelia.android.application"
-    elsif user_agent.match(/iphone/) || user_agent.match(/ipod/) || user_agent.match(/ipad/)
-      redirect_to "https://itunes.apple.com/fr/app/shopelia-lachat-facile-!/id731471392?mt=8"
-    else
-      redirect_to "/"
-    end
-  end
-
-  def connect
-    redirect_to session[:return_to] || home_index_path if current_user
   end
 end
